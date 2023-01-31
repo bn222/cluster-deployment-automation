746dafeaafd136a1db282a894fb0723cfb3c76ef
William Zhao <wizhao@redhat.com>
2023-01-31 18:05:04 -0500

    Add list argument to skip certain steps in deployment
    Signed-off-by: William Zhao <wizhao@redhat.com>

--- arguments.py
+++ arguments.py
@@ -6,10 +6,10 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Set up or tear down a (set of) clusters.')
     parser.add_argument('config', metavar='config', type=str, help='Yaml file with config')
     parser.add_argument('-t', '--teardown', dest='teardown', action='store_true', help='Remove anything that would be created by setting up the cluster(s)')
-    parser.add_argument('-s', '--skip-masters', dest='skipmasters', action='store_true', help='Don\'t deploy masters. Assume they have already been deployed')
-    parser.add_argument('-p', '--only-post', dest='onlypost', action='store_true', help='Only run post-config steps')
     parser.add_argument('--assisted-installer-url', dest='url', default='192.168.122.1', action='store', type=str, help='If set to 0.0.0.0 (the default), Assisted Installer will be started locally')
     parser.add_argument('--secret', dest='secrets_path', default='', action='store', type=str, help='pull_secret.json path (default is in cwd)')
+    steps = "pre,masters,workers,post"
+    parser.add_argument('-s', '--steps', dest='steps', type=str, default=steps, help=f'Comma-separated list of steps to run (by default: {steps}).')
 
     args = parser.parse_args()
 
