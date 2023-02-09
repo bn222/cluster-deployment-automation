ea67bf014b7eed7834b6c77eea28d1ae50df36e1
Balazs Nemeth <bnemeth@redhat.com>
2023-02-09 17:24:01 +0100

    use indentation of 4 in nfs

--- nfs.py
+++ nfs.py
@@ -5,15 +5,15 @@ NFS is needed in many cases to network mount the folder that contains
 ISO files such that Red Fish Virtual Media managers can load the image.
 """
 def export(path):
-  exports = "/etc/exports"
-  with open(exports, "r") as f:
-    contents = f.read()
-  for e in contents:
-    if e.split(" ")[0] == path:
-      return
-  contents += f"\n{path}"
-  with open(exports, "w") as f:
-    f.write(contents)
-  lh = host.LocalHost()
-  lh.run("systemctl enable nfs-server")
-  lh.run("systemctl restart nfs-server")
+    exports = "/etc/exports"
+    with open(exports, "r") as f:
+        contents = f.read()
+    for e in contents:
+        if path in e:
+            return
+    contents += f"\n{path}"
+    with open(exports, "w") as f:
+        f.write(contents)
+    lh = host.LocalHost()
+    lh.run("systemctl enable nfs-server")
+    lh.run("systemctl restart nfs-server")
