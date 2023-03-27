1c3380f54a75457cccff07e994c121090e7b761a
William Zhao <wizhao@redhat.com>
2023-03-27 16:37:23 -0400

    Fix minor ident issue in dependencies.sh
    Signed-off-by: William Zhao <wizhao@redhat.com>

--- dependencies.sh
+++ dependencies.sh
@@ -9,8 +9,8 @@ gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 dnf install -y rust coreos-installer kubectl libvirt podman qemu virt-install
 if ! command -v -- oc; then
-            export OPENSHIFT_CLIENT_TOOLS_URL=https://mirror.openshift.com/pub/openshift-v4/$(uname -m)/clients/ocp/stable/openshift-client-linux.tar.gz
-            curl $OPENSHIFT_CLIENT_TOOLS_URL | sudo tar -U -C /usr/local/bin -xzf -
-            fi
+    export OPENSHIFT_CLIENT_TOOLS_URL=https://mirror.openshift.com/pub/openshift-v4/$(uname -m)/clients/ocp/stable/openshift-client-linux.tar.gz
+    curl $OPENSHIFT_CLIENT_TOOLS_URL | sudo tar -U -C /usr/local/bin -xzf -
+fi
 
 cat requirements.txt  | xargs -n1 pip3 install
