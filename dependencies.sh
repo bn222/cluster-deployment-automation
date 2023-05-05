dac074825dbf61e1ff12ade7213180194d8741bd
William Zhao <wizhao@redhat.com>
2023-05-05 13:07:26 -0400

    Correct and add dependencies
    The pkg "qemu" doesn't exist in RHEL. We should install qemu-img and
    qemu-kvm instead.
    
    Signed-off-by: William Zhao <wizhao@redhat.com>

--- dependencies.sh
+++ dependencies.sh
@@ -1,3 +1,4 @@
+dnf install -y python3 pip
 pip3 install --upgrade pip
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
@@ -7,7 +8,7 @@ enabled=1
 gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
-dnf install -y rust coreos-installer kubectl libvirt podman qemu virt-install
+dnf install -y wget rust coreos-installer kubectl libvirt podman qemu-img qemu-kvm virt-install
 if ! command -v -- oc; then
     export OPENSHIFT_CLIENT_TOOLS_URL=https://mirror.openshift.com/pub/openshift-v4/$(uname -m)/clients/ocp/stable/openshift-client-linux.tar.gz
     curl $OPENSHIFT_CLIENT_TOOLS_URL | sudo tar -U -C /usr/local/bin -xzf -
