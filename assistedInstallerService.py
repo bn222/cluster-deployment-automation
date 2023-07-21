a92bcf9d23346f802186d97df74d451579eada49
Balazs Nemeth <bnemeth@redhat.com>
2023-07-21 09:37:26 +0200

    add support for 4.12.z and 4.12.0-nightly

--- assistedInstallerService.py
+++ assistedInstallerService.py
@@ -105,7 +105,7 @@ class AssistedInstallerService():
         return y
 
     def prep_version(self, version):
-        if re.search(r'4\.12\.[0-9]+-multi', version):
+        if re.search(r'4\.12\.[0-9]+', version):
             # Note how 4.12.0 has the -multi suffix because AI requires that
             # for 4.12. CDA hides this and simply expect 4.12.0 from the user
             # since that follows the same versioning scheme
@@ -113,6 +113,13 @@ class AssistedInstallerService():
               'openshift_version': f'{version}',
               'cpu_architectures': ['x86_64', 'arm64', 'ppc64le', 's390x'],
               'url': self.get_normal_pullspec(version.rstrip("-multi")),
+              'version': version,
+            }
+        elif re.search(r'4\.12\.0-nightly', version):
+            ret = {
+              'openshift_version': '4.12-multi',
+              'cpu_architectures': ['x86_64', 'arm64', 'ppc64le', 's390x'],
+              'url': self.get_nightly_pullspec(version),
               'version': version,
             }
         elif re.search(r'4\.13\.0-ec.[0-9]+', version):
