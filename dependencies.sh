a6a4cacc8fee2544293d0586b7ea5f80c87b9c47
Balazs Nemeth <bnemeth@redhat.com>
2023-08-31 15:23:03 +0200

    add virt-viewer dependency

--- dependencies.sh
+++ dependencies.sh
@@ -9,7 +9,7 @@ enabled=1
 gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
-dnf install -y wget rust coreos-installer kubectl libvirt podman qemu-img qemu-kvm virt-install make git golang-bin
+dnf install -y wget rust coreos-installer kubectl libvirt podman qemu-img qemu-kvm virt-install make git golang-bin virt-viewer
 if ! command -v -- oc; then
     export OPENSHIFT_CLIENT_TOOLS_URL=https://mirror.openshift.com/pub/openshift-v4/$(uname -m)/clients/ocp/stable/openshift-client-linux.tar.gz
     curl $OPENSHIFT_CLIENT_TOOLS_URL | sudo tar -U -C /usr/local/bin -xzf -
