bn222/cluster-deployment-automation

commit 318425bde60bde2b1ac9f149c673c547669ae9c3

Avoid issue when retrying to deploy same worker
Signed-off-by: Xavier Simonart <xsimonar@redhat.com>

clusterDeployer.py
@@ -35,6 +35,17 @@ def setup_vm(h: host.Host, virsh_pool: VirshPool, cfg: dict, iso_path: str):
     name = cfg["name"]
     ip = cfg["ip"]
     mac = "52:54:"+":".join(re.findall("..", secrets.token_hex()[:8]))
+
+    # If adding a worker node fails, one might want to retry w/o tearing down the whole cluster
+    # In that case, the DHCP entry might already be present, with wrong mac -> remove it
+    cmd = "virsh net-dumpxml default"
+    ret = h.run(cmd)
+    if name in ret.out:
+        logger.info(f"{name} already configured as static DHCP entry - removing before adding back with proper configuration")
+        host_xml = f"<host name='{name}'/>"
+        cmd = f"virsh net-update default delete ip-dhcp-host \"{host_xml}\" --live --config"
+        ret = h.run(cmd)
+
     host_xml = f"<host mac='{mac}' name='{name}' ip='{ip}'/>"
     logger.info(f"Creating static DHCP entry for VM {name}")
     cmd = f"virsh net-update default add ip-dhcp-host \"{host_xml}\" --live --config"

commit aed219865094b099e8eca4ed93be96133d872af5

Fix error trying to delete wrong DHCP entry
Before this patch, we were not looking for proper VM names
e.g. if we had 'bm-worker-20' as an entry, a search for for bm-worker-2
would have succeeded.

Fixes: 318425bde60b ("Avoid issue when retrying to deploy same worker")

Signed-off-by: Xavier Simonart <xsimonar@redhat.com>

clusterDeployer.py
@@ -40,7 +40,7 @@ def ensure_dhcp_entry(h: host.Host, name: str, ip: str, mac: str):
 
     cmd = "virsh net-dumpxml default"
     ret = h.run_or_die(cmd)
-    if name in ret.out:
+    if f"'{name}'" in ret.out:
         logger.info(f"{name} already configured as static DHCP entry - removing before adding back with proper configuration")
         host_xml = f"<host name='{name}'/>"
         cmd = f"virsh net-update default delete ip-dhcp-host \"{host_xml}\" --live --config"