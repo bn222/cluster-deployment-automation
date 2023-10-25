bn222/cluster-deployment-automation

commit b1d66d3a84e7bd7884842873b759a40b4b0cf0d0

clusterDeployer: Always update /etc/hosts with the current api_ip.
If the cluster name didn't change but the API IP did, the deployer
didn't update /etc/hosts.

Move the /etc/hosts update earlier in the cluster bring up to allow
accessing the API externally as early as possible.

Signed-off-by: Dumitru Ceara <dceara@redhat.com>

clusterDeployer.py
@@ -23,6 +23,7 @@ from nfs import NFS
 import coreosBuilder
 from typing import Tuple
 import common
+from python_hosts import Hosts, HostsEntry
 from logger import logger
 from extraConfigRunner import ExtraConfigRunner
 
@@ -576,6 +577,9 @@ class ClusterDeployer:
         self._wait_known_state(names, cb)
         self._ai.start_until_success(cluster_name)
 
+        logger.info('updating /etc/hosts')
+        self.update_etc_hosts()
+
         logger.info(f'downloading kubeconfig to {self._cc["kubeconfig"]}')
         self._ai.download_kubeconfig(self._cc["name"], self._cc["kubeconfig"])
 
@@ -585,7 +589,6 @@ class ClusterDeployer:
         self.ensure_linked_to_bridge(lh)
         for e in self._cc["masters"]:
             self._set_password(e)
-        self.update_etc_hosts()
 
     def _print_logs(self, name):
         ip = self._ai.get_ai_ip(name)
@@ -950,17 +953,12 @@ class ClusterDeployer:
         cluster_name = self._cc["name"]
         api_name = f"api.{cluster_name}.redhat.com"
         api_ip = self._ai.info_cluster(cluster_name).api_vip
-        found = False
-        etchost = ""
-        with open("/etc/hosts", "r") as f:
-            etchost = f.read()
-        for e in etchost.split("\n"):
-            if e and e.split()[1] == api_name:
-                found = True
-                break
-        if not found:
-            with open("/etc/hosts", "a") as f:
-                f.write(f"{api_ip} {api_name}\n")
+
+        hosts = Hosts()
+        hosts.remove_all_matching(name=api_name)
+        hosts.add([HostsEntry(entry_type='ipv4', address=api_ip, names=[api_name])])
+        hosts.write()
+
         # libvirtd also runs dnsmasq, and dnsmasq reads /etc/hosts.
         # For that reason, restart libvirtd to re-read the changes.
         lh = host.LocalHost()

commit ed49c46051c31de938ac7c955ff09c3f02ae9687

clusterDeployer: Update /etc/hosts after the AI cluster is up.
AI doesn't return a valid API VIP until at least one cluster node is up.
The safest bet right now is to wait until `wait_cluster()` returns.  At
that point nodes are up.

Fixes: b1d66d3a84e7 ("clusterDeployer: Always update /etc/hosts with the current api_ip.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

clusterDeployer.py
@@ -557,9 +557,6 @@ class ClusterDeployer:
         self._ai.ensure_infraenv_created(infra_env, cfg)
         self._ai.download_iso_with_retry(infra_env)
 
-        logger.info('updating /etc/hosts')
-        self.update_etc_hosts()
-
         lh = host.LocalHost()
         # TODO: clean this up. Currently just skipping this
         # since self.local_host_config() is not present if no local vms
@@ -584,6 +581,10 @@ class ClusterDeployer:
         self._ai.download_kubeconfig(self._cc["name"], self._cc["kubeconfig"])
 
         self._ai.wait_cluster(cluster_name)
+
+        logger.info('updating /etc/hosts')
+        self.update_etc_hosts()
+
         for p in futures:
             p.result()
         self.ensure_linked_to_bridge(lh)