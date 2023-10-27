f3205a258f136ab5341958e5df86421b1b5bb5a4
Balazs Nemeth <bnemeth@redhat.com>
2023-10-27 16:06:03 +0200

    further clean up cluster config in prep of mypy strict typing

--- clustersConfig.py
+++ clustersConfig.py
@@ -24,71 +24,71 @@ def current_host() -> str:
 
 class ClustersConfig:
     def __init__(self, yaml_path: str):
-        self._clusters: Optional[ClusterInfo] = None
+        self._cluster_info: Optional[ClusterInfo] = None
         self._current_host = current_host()
         self._load_full_config(yaml_path)
 
+        cc = self.fullConfig
         # Some config may be left out from the yaml. Try to provide defaults.
-        for cc in self.fullConfig["clusters"]:
-            if "masters" not in cc:
-                cc["masters"] = []
-            if "workers" not in cc:
-                cc["workers"] = []
-            if "kubeconfig" not in cc:
-                cc["kubeconfig"] = path.join(getcwd(), f'kubeconfig.{cc["name"]}')
-            if "preconfig" not in cc:
-                cc["preconfig"] = ""
-            if "postconfig" not in cc:
-                cc["postconfig"] = ""
-            if "version" not in cc:
-                cc["version"] = "4.13.0-ec.3"
-            if "external_port" not in cc:
-                cc["external_port"] = "auto"
-            if "network_api_port" not in cc:
-                cc["network_api_port"] = "auto"
-            if "proxy" not in cc:
-                cc["proxy"] = None
+        if "masters" not in cc:
+            cc["masters"] = []
+        if "workers" not in cc:
+            cc["workers"] = []
+        if "kubeconfig" not in cc:
+            cc["kubeconfig"] = path.join(getcwd(), f'kubeconfig.{cc["name"]}')
+        if "preconfig" not in cc:
+            cc["preconfig"] = ""
+        if "postconfig" not in cc:
+            cc["postconfig"] = ""
+        if "version" not in cc:
+            cc["version"] = "4.13.0-ec.3"
+        if "external_port" not in cc:
+            cc["external_port"] = "auto"
+        if "network_api_port" not in cc:
+            cc["network_api_port"] = "auto"
+        if "proxy" not in cc:
+            cc["proxy"] = None
 
-            if "hosts" not in cc:
-                cc["hosts"] = []
+        if "hosts" not in cc:
+            cc["hosts"] = []
 
-            # creates hosts entries for each referenced node name
-            all_nodes = cc["masters"] + cc["workers"]
-            for n in all_nodes:
-                if "disk_size" not in n:
-                    n["disk_size"] = 48
-                if "preallocated" not in n:
-                    n["preallocated"] = True
-                if "os_variant" not in n:
-                    n["os_variant"] = "rhel8.6"
+        # creates hosts entries for each referenced node name
+        all_nodes = cc["masters"] + cc["workers"]
+        for n in all_nodes:
+            if "disk_size" not in n:
+                n["disk_size"] = 48
+            if "preallocated" not in n:
+                n["preallocated"] = True
+            if "os_variant" not in n:
+                n["os_variant"] = "rhel8.6"
 
-            node_names = set(x["name"] for x in cc["hosts"])
-            for h in all_nodes:
-                if h["node"] not in node_names:
-                    cc["hosts"].append({"name": h["node"]})
-                    node_names.add(h["node"])
+        node_names = set(x["name"] for x in cc["hosts"])
+        for h in all_nodes:
+            if h["node"] not in node_names:
+                cc["hosts"].append({"name": h["node"]})
+                node_names.add(h["node"])
 
-            # Set default value for optional parameters for workers.
-            for node in all_nodes:
-                if "bmc_ip" not in node:
-                    node["bmc_ip"] = None
-                if "bmc_user" not in node:
-                    node["bmc_user"] = "root"
-                if "bmc_password" not in node:
-                    node["bmc_password"] = "calvin"
-                if "image_path" not in node:
-                    base_path = f'/home/{cc["name"]}_guests_images'
-                    qemu_img_name = f'{node["name"]}.qcow2'
-                    node["image_path"] = os.path.join(base_path, qemu_img_name)
-            for host_config in cc["hosts"]:
-                if "network_api_port" not in host_config:
-                    host_config["network_api_port"] = cc["network_api_port"]
-                if "username" not in host_config:
-                    host_config["username"] = "core"
-                if "password" not in host_config:
-                    host_config["password"] = None
-                if "pre_installed" not in host_config:
-                    host_config["pre_installed"] = "True"
+        # Set default value for optional parameters for workers.
+        for node in all_nodes:
+            if "bmc_ip" not in node:
+                node["bmc_ip"] = None
+            if "bmc_user" not in node:
+                node["bmc_user"] = "root"
+            if "bmc_password" not in node:
+                node["bmc_password"] = "calvin"
+            if "image_path" not in node:
+                base_path = f'/home/{cc["name"]}_guests_images'
+                qemu_img_name = f'{node["name"]}.qcow2'
+                node["image_path"] = os.path.join(base_path, qemu_img_name)
+        for host_config in cc["hosts"]:
+            if "network_api_port" not in host_config:
+                host_config["network_api_port"] = cc["network_api_port"]
+            if "username" not in host_config:
+                host_config["username"] = "core"
+            if "password" not in host_config:
+                host_config["password"] = None
+            if "pre_installed" not in host_config:
+                host_config["pre_installed"] = "True"
 
     def _load_full_config(self, yaml_path: str) -> None:
         if not path.exists(yaml_path):
@@ -117,19 +117,19 @@ class ClustersConfig:
     def _apply_jinja(self, contents: str) -> str:
         def worker_number(a):
             self._ensure_clusters_loaded()
-            assert self._clusters is not None
-            name = self._clusters.workers[a]
+            assert self._cluster_info is not None
+            name = self._cluster_info.workers[a]
             return re.sub("[^0-9]", "", name)
 
         def worker_name(a):
             self._ensure_clusters_loaded()
-            assert self._clusters is not None
-            return self._clusters.workers[a]
+            assert self._cluster_info is not None
+            return self._cluster_info.workers[a]
 
         def api_network():
             self._ensure_clusters_loaded()
-            assert self._clusters is not None
-            return self._clusters.network_api_port
+            assert self._cluster_info is not None
+            return self._cluster_info.network_api_port
 
         format_string = contents
 
@@ -145,12 +145,12 @@ class ClustersConfig:
         return t
 
     def _ensure_clusters_loaded(self) -> None:
-        if self._clusters is not None:
+        if self._cluster_info is not None:
             return
         all_cluster_info = load_all_cluster_info()
         ch = current_host()
         if ch in all_cluster_info:
-            self._clusters = all_cluster_info[ch]
+            self._cluster_info = all_cluster_info[ch]
         else:
             sys.exit(-1)
 
