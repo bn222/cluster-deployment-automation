d1c2466e86bf2366657e202963831ce218f2e2cd
Zinkelburger <anbernal@redhat.com>
2024-01-19 14:31:56 -0500

    added check for if the script is in the venv or not. Can use venv python or python3.11

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,16 @@
-# assumes venv was created with `python3.11 -m venv ocp-venv`
-python -m ensurepip --upgrade
-python -m pip install PyYAML --ignore-installed
+#!/bin/bash
+
+if [ -z "$VIRTUAL_ENV" ]; then
+    PYTHON_CMD="python3.11"
+    sudo dnf install -y python3.11
+else
+    # assumes venv was created with `python3.11 -m venv ocp-venv`
+    PYTHON_CMD="python"
+fi
+
+$PYTHON_CMD -m ensurepip --upgrade
+$PYTHON_CMD -m pip install PyYAML --ignore-installed
+
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -9,12 +19,14 @@ enabled=1
 gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
+
 dnf install -y wget rust coreos-installer kubectl libvirt podman qemu-img qemu-kvm virt-install make git golang-bin virt-viewer
+
 if ! command -v -- oc; then
     export OPENSHIFT_CLIENT_TOOLS_URL=https://mirror.openshift.com/pub/openshift-v4/$(uname -m)/clients/ocp/stable/openshift-client-linux.tar.gz
     curl $OPENSHIFT_CLIENT_TOOLS_URL | sudo tar -U -C /usr/local/bin -xzf -
 fi
 
-cat requirements.txt  | xargs -n1 python -m pip install
+cat requirements.txt  | xargs -n1 $PYTHON_CMD -m pip install
 
 sudo activate-global-python-argcomplete
