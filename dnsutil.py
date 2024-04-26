bn222/cluster-deployment-automation

commit 784959b955648436c6d164b96eb161eeb65894c4

clusterDeployer: configure dnsmasq instance for cluster DNS
It is rather useful to be able to resolve the DNS names of the cluster.
For example, routes (`oc expose`) are vhost http proxies that rely on
correct DNS name.

Setup dnsmasq by dropping a snippet "/etc/dnsmasq.d/cda-{cluster_name}.conf"
and start the service.

Also update /etc/resolv.conf to "nameserver 127.0.0.1".

You can opt-out from changing "/etc/resolv.conf" with

    sudo touch /etc/.resolv.conf.cda-dont-touch

Try:

  $ CDA_LOG_LEVEL=debug python -c 'import clusterDeployer as c; c._dnsmasq_update("cluster", "192.168.99.5")'

dnsutil.py
@@ -1,0 +1,216 @@
+import dataclasses
+import os
+
+import common
+import host
+import logging
+
+from logger import logger
+from typing import Optional
+
+
+RESOLVCONF = "/etc/resolv.conf"
+RESOLVCONF_ORIG = "/etc/resolv.conf.cda-orig"
+RESOLVCONF_LOCAL = "/etc/resolv.conf.cda-local"
+
+
+@dataclasses.dataclass
+class ResolvConfData:
+    nameservers: list[str]
+    searches: list[str]
+
+
+def resolvconf_parse_file(rc_file: str) -> ResolvConfData:
+    try:
+        with open(rc_file, "rb") as f:
+            rc_content = f.read()
+    except IOError:
+        return ResolvConfData([], [])
+
+    def _startswith(line: bytes, prefix: bytes) -> Optional[str]:
+        if not line.startswith(prefix):
+            return None
+        length = len(prefix)
+        if len(line) <= length:
+            return None
+        if line[length] not in (32, 9):
+            return None
+        s = line[length + 1 :].strip()
+        try:
+            return s.decode()
+        except ValueError:
+            return None
+
+    nameservers = []
+    searches = []
+    for line in rc_content.split(b'\n'):
+        line = line.strip()
+        if (val := _startswith(line, b"nameserver")) is not None:
+            addr = common.ipaddr_norm(val)
+            if not addr:
+                continue
+            if addr not in nameservers:
+                nameservers.append(addr)
+        elif (val := _startswith(line, b"search")) is not None:
+            # Keep the last occurrence. `man resolv.conf` says "If there are
+            # multiple search directives, only the search list from the last
+            # instance is used".
+            searches = val.split()
+
+    return ResolvConfData(nameservers, searches)
+
+
+def resolvconf_ensure_orig() -> None:
+
+    if os.path.exists(RESOLVCONF_ORIG):
+        return
+
+    rc_content_orig = b""
+    try:
+        with open(RESOLVCONF, "rb") as f:
+            rc_content_orig = f.read()
+    except IOError:
+        pass
+    # Backup the original resolv.conf file to "RESOLVCONF_ORIG"
+    if rc_content_orig == b"":
+        pass
+    elif b"Written by cluster-deployment-automation" in rc_content_orig:
+        rc_content_orig = b""
+    elif b"Generated by NetworkManager" in rc_content_orig and os.path.exists("/run/NetworkManager/resolv.conf"):
+        # This file seems to be written by NetworkManager. Symlink there.
+        logger.info(f"resolv.conf: point {RESOLVCONF_ORIG} to /run/NetworkManager/resolv.conf")
+        lh = host.LocalHost()
+        lh.run_or_die(f"ln -snf /run/NetworkManager/resolv.conf {RESOLVCONF_ORIG}")
+    else:
+        logger.info(f"resolv.conf: write {RESOLVCONF_ORIG}")
+        with common.atomic_write(RESOLVCONF_ORIG, text=False) as f:
+            f.write(rc_content_orig)
+
+
+def resolvconf_update(setup: bool = True, searches: Optional[list[str]] = None) -> None:
+
+    lh = host.LocalHost()
+
+    # The user can disable touching /etc/resolv.conf by
+    #
+    #   $ sudo touch /etc/.resolv.conf.cda-dont-touch
+    #
+    # We will still write our "/etc/resolv.conf.cda*" files, but
+    # it's up to the user to redirect symlinks accordingly.
+    dont_touch = os.path.exists("/etc/.resolv.conf.cda-dont-touch")
+
+    if not setup:
+        if dont_touch:
+            return
+        if not os.path.exists(RESOLVCONF_ORIG):
+            return
+        try:
+            lnk = os.readlink(RESOLVCONF)
+        except Exception:
+            return
+        if not os.path.basename(lnk).startswith("resolv.conf.cda."):
+            # We only restore, if /etc/resolv.conf is a symlink to "resolv.conf-cda.*"
+            # which indicates that the file was created by us.
+            return
+        dst = os.path.basename(RESOLVCONF_ORIG)
+        if lnk == dst:
+            return
+        logger.info(f"resolv.conf: restore /etc/resolv.conf to link to original {RESOLVCONF_ORIG}")
+        lh.run_or_die(f"ln -snf {dst} {RESOLVCONF}")
+        return
+
+    searches_str = ""
+    if searches:
+        searches_str = f"search {' '.join(searches)}\n"
+    with common.atomic_write(RESOLVCONF_LOCAL) as file:
+        file.write(
+            f"""# Written by cluster-deployment-automation.
+#
+# A local dnsmasq instance is configured (`systemctl status dnsmasq.service`).
+# Original file is at "{RESOLVCONF_ORIG}".
+#
+# Set:     `sudo ln -snf {os.path.basename(RESOLVCONF_LOCAL)} /etc/resolv.conf`
+# Restore: `sudo ln -snf {os.path.basename(RESOLVCONF_ORIG)} /etc/resolv.conf`
+# Alternatively, restore by deleting "/etc/resolv.conf" and let NetworkManager
+# write anew with `systemctl reload NetworkManager.service`.
+#
+# Prevent touching "/etc/resolv.conf" with a file "/etc/.resolv.conf.cda-dont-touch"
+
+{searches_str}nameserver 127.0.0.1
+"""
+        )
+    # We only point a symlink to the correct file. This indicates that
+    # somebody is handling the file and NetworkManager leaves the symlink
+    # alone.
+    # It's also easy to restore the previous file with
+    # `ln -snf resolv.conf.cda-orig /etc/resolv.conf`.
+    if not dont_touch:
+        lh.run_or_die(f"ln -snf {os.path.basename(RESOLVCONF_LOCAL)} {RESOLVCONF}")
+
+
+def dnsmasq_update(cluster_name: str, api_vip: Optional[str] = None) -> None:
+
+    lh = host.LocalHost()
+
+    service_running = lh.run("systemctl is-active --quiet dnsmasq.service").success()
+
+    service_restart = True
+
+    dmasqconf_my = f"/etc/dnsmasq.d/cda-cluster-{cluster_name}.conf"
+    dmasqconf_orig = "/etc/dnsmasq.d/cda-orig.conf"
+
+    if api_vip is None:
+        loglevel = logging.INFO
+        try:
+            os.remove(dmasqconf_my)
+        except FileNotFoundError:
+            loglevel = logging.DEBUG
+            service_restart = False
+        except OSError:
+            service_restart = service_running
+        logger.log(loglevel, f"dnsmasq: reset local dnsmasq by deleting {dmasqconf_my}")
+    else:
+        content = f"# Written by cluster-deployment-automation for \"{cluster_name}\"\nserver=/{cluster_name}.redhat.com/{api_vip}\n"
+
+        try:
+            with open(dmasqconf_my) as file:
+                old_content = file.read()
+        except IOError:
+            old_content = ""
+
+        if content == old_content:
+            logger.debug(f"dnsmasq: updating file {dmasqconf_my} (nameserver: {api_vip})")
+            service_restart = not service_running
+        else:
+            logger.info(f"dnsmasq: updating file {dmasqconf_my} (nameserver: {api_vip})")
+            with common.atomic_write(dmasqconf_my) as file:
+                file.write(content)
+
+    resolvconf_ensure_orig()
+    rcdata = resolvconf_parse_file(RESOLVCONF_ORIG)
+
+    if api_vip is None:
+        # We don't reset. That's because we might have multiple clusters. If
+        # you want to reset /etc/resolv.conf, see comments in
+        # "/etc/resolv.conf.cda-local".
+        pass
+    else:
+        resolvconf_update(searches=rcdata.searches)
+
+    with common.atomic_write(dmasqconf_orig) as file:
+        content = "# Written by cluster-deployment-automation.\n"
+        content += "# Contains the nameserves from \"/etc/resolv.conf.cda-orig\".\n"
+        content += ''.join(f"server={n}\n" for n in rcdata.nameservers)
+        content += "listen-address=127.0.0.1\n"
+        content += "bind-interfaces\n"
+        file.write(content)
+
+    if service_restart:
+        ret = lh.run("systemctl unmask dnsmasq.service")
+        ret = lh.run("systemctl restart dnsmasq.service")
+        if not ret.success():
+            logger.warning(f"dnsmasq: failure to start dnsmasq service: {ret}")
+        else:
+            logger.debug("dnsmasq: service was restarted")
+    else:
+        logger.debug("dnsmasq: service was not restarted")

clusterDeployer.py
@@ -22,6 +22,7 @@ from logger import logger
 import microshift
 from extraConfigRunner import ExtraConfigRunner
 from clusterHost import ClusterHost
+import dnsutil
 
 
 def match_to_proper_version_format(version_cluster_config: str) -> str:
@@ -103,6 +104,8 @@ class ClusterDeployer:
         cluster_name = self._cc.name
         logger.info(f"Tearing down {cluster_name}")
         self._ai.ensure_cluster_deleted(self._cc.name)
+
+        self.update_dnsmasq(setup=False)
 
         for h in self._all_hosts:
             h.teardown()
@@ -352,6 +355,8 @@ class ClusterDeployer:
             for master in h.k8s_master_nodes:
                 master.set_password()
 
+        self.update_dnsmasq()
+
     def create_workers(self) -> None:
         logger.info("Setting up workers")
         cluster_name = self._cc.name
@@ -541,6 +546,14 @@ class ClusterDeployer:
         # For that reason, restart libvirtd to re-read the changes.
         lh = host.LocalHost()
         lh.run("systemctl restart libvirtd")
+
+    def update_dnsmasq(self, *, setup: bool = True) -> None:
+        cluster_name = self._cc.name
+        if setup:
+            api_vip = self._ai.get_ai_cluster_info(cluster_name).api_vip
+        else:
+            api_vip = None
+        dnsutil.dnsmasq_update(cluster_name, api_vip)
 
     def wait_for_workers(self) -> None:
         logger.info(f'waiting for {len(self._cc.workers)} workers')

test_dnsutil.py
@@ -1,0 +1,25 @@
+import pathlib
+
+import dnsutil
+
+
+def test_resolv_conf_parse_file(tmp_path: pathlib.Path) -> None:
+    def _rc(content: str | bytes) -> tuple[list[str], list[str]]:
+        filename = str(tmp_path / "rcfile")
+        if isinstance(content, str):
+            content = content.encode('utf-8')
+        with open(filename, "wb") as f:
+            f.write(content)
+        rcdata = dnsutil.resolvconf_parse_file(filename)
+        return (rcdata.nameservers, rcdata.searches)
+
+    assert _rc("nameserver  1.2.3.4") == (["1.2.3.4"], [])
+    assert _rc("nameserver  1::04") == (["1::4"], [])
+    assert _rc("nameserver 1::04") == (["1::4"], [])
+    assert _rc("nameserver1::04") == ([], [])
+    assert _rc("nameserver\t1::04") == (["1::4"], [])
+    assert _rc("\n\tnameserver\t1::04\t\n") == (["1::4"], [])
+    assert _rc(b"nameserver\t1::0\xca4") == ([], [])
+    assert _rc(b"nameserver\t1::0\xca4\nnameserver 1.2.3.4  ") == (["1.2.3.4"], [])
+    assert _rc("search foo.com bar.com\nnameserver  1.2.3.4\n") == (["1.2.3.4"], ["foo.com", "bar.com"])
+    assert _rc("search foo.com bar.com\n\nsearch xxx\nnameserver  1.2.3.4\n") == (["1.2.3.4"], ["xxx"])

commit 03f05ec595ee9e6b09b1b3fafc43f5075ff92f24

dns: ensure libvirt's dnsmasq does not forward to the local dnsmasq
We now run a local dnsmasq instance, which can resolve DNS names of the
cluster.

Note that libvirt's network also runs a dnsmasq instance, which by
default reads /etc/resolv.conf to get upstream nameservers.

If you previously already deployed a cluster, then /etc/resolv.conf
contains "nameserver 127.0.0.1". If libvirt's dnsmasq takes that as
upstream, queries to the network may end up in a loop and we see

  dnsmasq[430088]: Maximum number of concurrent DNS queries reached (max: 150)

Instead, use the original "/etc/resolv.conf.cda-orig" file, which
contains the original upstream DNS servers.

https://libvirt.org/formatnetwork.html#network-namespaces

Fixes: 784959b95564 ('clusterDeployer: configure dnsmasq instance for cluster DNS')

dnsutil.py
@@ -20,6 +20,16 @@ class ResolvConfData:
     searches: list[str]
 
 
+def resolvconf_dont_touch() -> bool:
+    # The user can disable touching /etc/resolv.conf by
+    #
+    #   $ sudo touch /etc/.resolv.conf.cda-dont-touch
+    #
+    # We will still write our "/etc/resolv.conf.cda*" files, but
+    # it's up to the user to redirect symlinks accordingly.
+    return os.path.exists("/etc/.resolv.conf.cda-dont-touch")
+
+
 def resolvconf_parse_file(rc_file: str) -> ResolvConfData:
     try:
         with open(rc_file, "rb") as f:
@@ -60,7 +70,7 @@ def resolvconf_parse_file(rc_file: str) -> ResolvConfData:
     return ResolvConfData(nameservers, searches)
 
 
-def resolvconf_ensure_orig() -> None:
+def _resolvconf_ensure_orig() -> None:
 
     if os.path.exists(RESOLVCONF_ORIG):
         return
@@ -87,6 +97,15 @@ def resolvconf_ensure_orig() -> None:
             f.write(rc_content_orig)
 
 
+def resolvconf_ensure_orig() -> bool:
+
+    if resolvconf_dont_touch():
+        return False
+
+    _resolvconf_ensure_orig()
+    return True
+
+
 def resolvconf_update(setup: bool = True, searches: Optional[list[str]] = None) -> None:
 
     lh = host.LocalHost()
@@ -97,7 +116,7 @@ def resolvconf_update(setup: bool = True, searches: Optional[list[str]] = None) 
     #
     # We will still write our "/etc/resolv.conf.cda*" files, but
     # it's up to the user to redirect symlinks accordingly.
-    dont_touch = os.path.exists("/etc/.resolv.conf.cda-dont-touch")
+    dont_touch = resolvconf_dont_touch()
 
     if not setup:
         if dont_touch:
@@ -186,7 +205,7 @@ def dnsmasq_update(cluster_name: str, api_vip: Optional[str] = None) -> None:
             with common.atomic_write(dmasqconf_my) as file:
                 file.write(content)
 
-    resolvconf_ensure_orig()
+    _resolvconf_ensure_orig()
     rcdata = resolvconf_parse_file(RESOLVCONF_ORIG)
 
     if api_vip is None:

virtualBridge.py
@@ -7,6 +7,7 @@ from typing import Optional
 
 import common
 import host
+import dnsutil
 from clustersConfig import BridgeConfig, NodeConfig
 
 
@@ -96,7 +97,7 @@ class VirBridge:
         if api_port is not None:
             self.hostconn.run(f"ip link set {api_port} up")
 
-    def _network_xml(self) -> str:
+    def _network_xml(self, use_resolvconf_orig: bool = False) -> str:
         if self.config.dynamic_ip_range is None:
             dhcp_part = ""
         else:
@@ -104,14 +105,20 @@ class VirBridge:
                 {bridge_dhcp_range_str(self.config.dynamic_ip_range)}
                 </dhcp>"""
 
+        if use_resolvconf_orig:
+            dnsmasq_options = f"<dnsmasq:option value=\"resolv-file={dnsutil.RESOLVCONF_ORIG}\" />"
+        else:
+            dnsmasq_options = ""
+
         return f"""
-                <network>
+                <network xmlns:dnsmasq='http://libvirt.org/schemas/network/dnsmasq/1.0'>
                 <name>default</name>
                 <forward mode='nat'/>
                 <bridge name='virbr0' stp='off' delay='0'/>
                 {bridge_ip_address_str(self.config.ip, self.config.mask)}
                 {dhcp_part}
                 </ip>
+                <dnsmasq:options>{dnsmasq_options}</dnsmasq:options>
                 </network>"""
 
     def _restart(self) -> None:
@@ -125,6 +132,17 @@ class VirBridge:
         self._restart()
 
     def configure(self, api_port: Optional[str]) -> None:
+
+        # We may redirect /etc/resolv.conf to 127.0.0.1. See dnsmasq.dnsmasq_update().
+        # Note that libvirt's dnsmasq also reads /etc/resolv.conf, but that works badly,
+        # if that points to our local dnsmasq (which in turn might point to a DNS server
+        # inside a libvirt VM).
+        #
+        # Instead, we have the original nameservers in /etc/resolv.conf.cda-orig.
+        # Use that for the "--resolv-file=" option of libvirt's dnsmasq.
+        # https://libvirt.org/formatnetwork.html#network-namespaces
+        use_resolvconf_orig = dnsutil.resolvconf_ensure_orig()
+
         hostname = self.hostconn.hostname()
         cmd = "systemctl enable libvirtd --now"
         self.hostconn.run_or_die(cmd)
@@ -154,10 +172,13 @@ class VirBridge:
         if bridge_ip_address_str(self.config.ip, self.config.mask) not in ret.out:
             needs_reconfigure = True
 
+        if use_resolvconf_orig != (f"\"resolv-file={dnsutil.RESOLVCONF_ORIG}\"" in ret.out):
+            needs_reconfigure = True
+
         if needs_reconfigure:
             logger.info("Destoying and recreating bridge")
             logger.info(f"creating default-net.xml on {hostname}")
-            contents = self._network_xml()
+            contents = self._network_xml(use_resolvconf_orig)
 
             bridge_xml = os.path.join("/tmp", 'vir_bridge.xml')
             self.hostconn.write(bridge_xml, contents)