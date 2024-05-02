bn222/cluster-deployment-automation

commit 03f05ec595ee9e6b09b1b3fafc43f5075ff92f24

dns: ensure libvirt's dnsmasq does not forward to the local dnsmasq
We now run a local dnsmasq instance, which can resolve DNS names of the
cluster.

Note that libvirt's network also runs a dnsmasq instance, which by
default reads /etc/resolv.conf to get upstream nameservers.

If you previously already deployed a cluster, then /etc/resolv.conf
contains "nameserver 127.0.0.1". If libvirt's dnsmasq takes that as
upstream, queries to the network may end up in a loop and we see

  dnsmasq[430088]: Maximum number of concurrent DNS queries reached (max: 150)

Instead, use the original "/etc/resolv.conf.cda-orig" file, which
contains the original upstream DNS servers.

https://libvirt.org/formatnetwork.html#network-namespaces

Fixes: 784959b95564 ('clusterDeployer: configure dnsmasq instance for cluster DNS')

virtualBridge.py
@@ -7,6 +7,7 @@ from typing import Optional
 
 import common
 import host
+import dnsutil
 from clustersConfig import BridgeConfig, NodeConfig
 
 
@@ -96,7 +97,7 @@ class VirBridge:
         if api_port is not None:
             self.hostconn.run(f"ip link set {api_port} up")
 
-    def _network_xml(self) -> str:
+    def _network_xml(self, use_resolvconf_orig: bool = False) -> str:
         if self.config.dynamic_ip_range is None:
             dhcp_part = ""
         else:
@@ -104,14 +105,20 @@ class VirBridge:
                 {bridge_dhcp_range_str(self.config.dynamic_ip_range)}
                 </dhcp>"""
 
+        if use_resolvconf_orig:
+            dnsmasq_options = f"<dnsmasq:option value=\"resolv-file={dnsutil.RESOLVCONF_ORIG}\" />"
+        else:
+            dnsmasq_options = ""
+
         return f"""
-                <network>
+                <network xmlns:dnsmasq='http://libvirt.org/schemas/network/dnsmasq/1.0'>
                 <name>default</name>
                 <forward mode='nat'/>
                 <bridge name='virbr0' stp='off' delay='0'/>
                 {bridge_ip_address_str(self.config.ip, self.config.mask)}
                 {dhcp_part}
                 </ip>
+                <dnsmasq:options>{dnsmasq_options}</dnsmasq:options>
                 </network>"""
 
     def _restart(self) -> None:
@@ -125,6 +132,17 @@ class VirBridge:
         self._restart()
 
     def configure(self, api_port: Optional[str]) -> None:
+
+        # We may redirect /etc/resolv.conf to 127.0.0.1. See dnsmasq.dnsmasq_update().
+        # Note that libvirt's dnsmasq also reads /etc/resolv.conf, but that works badly,
+        # if that points to our local dnsmasq (which in turn might point to a DNS server
+        # inside a libvirt VM).
+        #
+        # Instead, we have the original nameservers in /etc/resolv.conf.cda-orig.
+        # Use that for the "--resolv-file=" option of libvirt's dnsmasq.
+        # https://libvirt.org/formatnetwork.html#network-namespaces
+        use_resolvconf_orig = dnsutil.resolvconf_ensure_orig()
+
         hostname = self.hostconn.hostname()
         cmd = "systemctl enable libvirtd --now"
         self.hostconn.run_or_die(cmd)
@@ -154,10 +172,13 @@ class VirBridge:
         if bridge_ip_address_str(self.config.ip, self.config.mask) not in ret.out:
             needs_reconfigure = True
 
+        if use_resolvconf_orig != (f"\"resolv-file={dnsutil.RESOLVCONF_ORIG}\"" in ret.out):
+            needs_reconfigure = True
+
         if needs_reconfigure:
             logger.info("Destoying and recreating bridge")
             logger.info(f"creating default-net.xml on {hostname}")
-            contents = self._network_xml()
+            contents = self._network_xml(use_resolvconf_orig)
 
             bridge_xml = os.path.join("/tmp", 'vir_bridge.xml')
             self.hostconn.write(bridge_xml, contents)

dnsutil.py
@@ -20,6 +20,16 @@ class ResolvConfData:
     searches: list[str]
 
 
+def resolvconf_dont_touch() -> bool:
+    # The user can disable touching /etc/resolv.conf by
+    #
+    #   $ sudo touch /etc/.resolv.conf.cda-dont-touch
+    #
+    # We will still write our "/etc/resolv.conf.cda*" files, but
+    # it's up to the user to redirect symlinks accordingly.
+    return os.path.exists("/etc/.resolv.conf.cda-dont-touch")
+
+
 def resolvconf_parse_file(rc_file: str) -> ResolvConfData:
     try:
         with open(rc_file, "rb") as f:
@@ -60,7 +70,7 @@ def resolvconf_parse_file(rc_file: str) -> ResolvConfData:
     return ResolvConfData(nameservers, searches)
 
 
-def resolvconf_ensure_orig() -> None:
+def _resolvconf_ensure_orig() -> None:
 
     if os.path.exists(RESOLVCONF_ORIG):
         return
@@ -87,6 +97,15 @@ def resolvconf_ensure_orig() -> None:
             f.write(rc_content_orig)
 
 
+def resolvconf_ensure_orig() -> bool:
+
+    if resolvconf_dont_touch():
+        return False
+
+    _resolvconf_ensure_orig()
+    return True
+
+
 def resolvconf_update(setup: bool = True, searches: Optional[list[str]] = None) -> None:
 
     lh = host.LocalHost()
@@ -97,7 +116,7 @@ def resolvconf_update(setup: bool = True, searches: Optional[list[str]] = None) 
     #
     # We will still write our "/etc/resolv.conf.cda*" files, but
     # it's up to the user to redirect symlinks accordingly.
-    dont_touch = os.path.exists("/etc/.resolv.conf.cda-dont-touch")
+    dont_touch = resolvconf_dont_touch()
 
     if not setup:
         if dont_touch:
@@ -186,7 +205,7 @@ def dnsmasq_update(cluster_name: str, api_vip: Optional[str] = None) -> None:
             with common.atomic_write(dmasqconf_my) as file:
                 file.write(content)
 
-    resolvconf_ensure_orig()
+    _resolvconf_ensure_orig()
     rcdata = resolvconf_parse_file(RESOLVCONF_ORIG)
 
     if api_vip is None:

commit 303e252295da0bb940fe3f12d11cd4e0598e4c81

virtualBridge: Fix matching of resolv-file contents in the bridge config.
There's no guarantee that "virsh net-define <file>" doesn't replace
double quotes with single quotes.  For example, on RHEL 9.3:

  $ virsh --version
  9.5.0

  $  cat /tmp/vir_bridge.xml | grep resolv-file
     <dnsmasq:options><dnsmasq:option value="resolv-file=/etc/resolv.conf.cda-orig" /></dnsmasq:options>

  $ virsh net-define /tmp/vir_bridge.xml
  Network default defined from /tmp/vir_bridge.xml

  $ virsh net-dumpxml default | grep resolv-file
    <dnsmasq:option value='resolv-file=/etc/resolv.conf.cda-orig'/>

Fixes: 03f05ec595ee ("dns: ensure libvirt's dnsmasq does not forward to the local dnsmasq")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

virtualBridge.py
@@ -172,7 +172,7 @@ class VirBridge:
         if bridge_ip_address_str(self.config.ip, self.config.mask) not in ret.out:
             needs_reconfigure = True
 
-        if use_resolvconf_orig != (f"\"resolv-file={dnsutil.RESOLVCONF_ORIG}\"" in ret.out):
+        if use_resolvconf_orig != (f"resolv-file={dnsutil.RESOLVCONF_ORIG}" in ret.out):
             needs_reconfigure = True
 
         if needs_reconfigure: