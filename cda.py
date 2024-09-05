bn222/cluster-deployment-automation

commit ad13ce19afd2e76033a7dd188f848413d553f279

Propagate secret settings

cda.py
@@ -51,7 +51,7 @@ def main_deploy_iso(cc: ClustersConfig, args: argparse.Namespace) -> None:
 
 
 def main_deploy(args: argparse.Namespace) -> None:
-    cc = ClustersConfig(args.config, args.worker_range)
+    cc = ClustersConfig(args.config, args.secrets_path, args.worker_range)
 
     if cc.kind == "openshift":
         main_deploy_openshift(cc, args)

clustersConfig.py
@@ -164,21 +164,19 @@ class ClustersConfig:
     ntp_source: str = "clock.redhat.com"
     base_dns_domain: str = "redhat.com"
     install_iso: str = ""
+    secrets_path: str = ""
 
     # All configurations that used to be supported but are not anymore.
     # Used to warn the user to change their config.
     deprecated_configs: dict[str, Optional[str]] = {"api_ip": "api_vip", "ingress_ip": "ingress_vip"}
 
-    def __init__(
-        self,
-        yaml_path: str,
-        worker_range: common.RangeList = common.RangeList.UNLIMITED,
-    ):
+    def __init__(self, yaml_path: str, secrets_path: str, worker_range: common.RangeList = common.RangeList.UNLIMITED):
         self._cluster_info: Optional[ClusterInfo] = None
         self._load_full_config(yaml_path)
         self._check_deprecated_config()
 
         cc = self.fullConfig
+        self.secrets_path = secrets_path
         self.set_cc_defaults(cc)
         if "proxy" in cc:
             self.proxy = cc["proxy"]

extraConfigMicroshift.py
@@ -76,7 +76,7 @@ def ExtraConfigMicroshift(cc: ClustersConfig, cfg: ExtraConfigArgs, futures: dic
     # Set up pull secret
     logger.info(f"Copying pull secret to {acc.hostname()}:/etc/crio/openshift-pull-secret")
     acc.run("mkdir -p /etc/crio")
-    acc.copy_to("pull_secret.json", "/etc/crio/openshift-pull-secret")
+    acc.copy_to(cc.secrets_path, "/etc/crio/openshift-pull-secret")
     acc.run_or_die("chown root:root /etc/crio/openshift-pull-secret")
     acc.run_or_die("chmod 600 /etc/crio/openshift-pull-secret")
 

microshift.py
@@ -63,8 +63,8 @@ def cleanup_compose_cli(h: host.Host) -> None:
 
 
 # uses jinja to generate a kickstart file
-def generate_kickstart(rhel_number: str, uname_m: str) -> None:
-    with open('pull_secret.json', 'r') as f_in:
+def generate_kickstart(rhel_number: str, uname_m: str, secrets_path: str) -> None:
+    with open(secrets_path, 'r') as f_in:
         file_contents = f_in.read()
 
     ssh_pub, _, _ = next(common.iterate_ssh_keys(), (None, None, None))
@@ -143,13 +143,13 @@ def wait_for_build_to_finish(h: host.Host, build_id_two: Optional[str] = None) -
 
 
 # generates an iso file that builds microshift
-def iso_builder(h: host.Host, name_of_final_iso: str, version: str) -> None:
+def iso_builder(h: host.Host, name_of_final_iso: str, secrets_path: str, version: str) -> None:
     rhel_number = '9'
     cmd = "uname -m"
     uname_m = h.run(cmd).out.strip()
 
     cleanup_microshift(h, version)
-    generate_kickstart(rhel_number, uname_m)
+    generate_kickstart(rhel_number, uname_m, secrets_path)
 
     rhel_version = 'rhel9'
     rhel_name = 'RHEL 9'
@@ -216,14 +216,14 @@ rhsm = true'''.strip()
     shutil.rmtree("/var/cache/osbuild-worker/osbuild-store/stage/")
 
 
-def deploy(cluster_name: str, node: NodeConfig, external_port: str, version: str) -> None:
+def deploy(secrets_path: str, node: NodeConfig, external_port: str, version: str) -> None:
     lh = host.LocalHost()
     bmc = BMC.from_bmc(node.bmc, node.bmc_user, node.bmc_password)
     h = Host(node.node, bmc)
     name_of_final_iso = os.path.join(os.getcwd(), 'final.iso')
     login_uname = "redhat"
 
-    iso_builder(lh, name_of_final_iso, version)
+    iso_builder(lh, name_of_final_iso, secrets_path, version)
 
     if os.path.exists(f"{name_of_final_iso}"):
         logger.info(f"The file {name_of_final_iso} exists.")

commit acfa0a9b04e7eefe74abb644ed1de5666d231f06

cda: fix passing secrets_path to ClustersConfig
"secrets_path" was a mandatory positional argument. "main_snapshot()"
missed to pass that argument on. Fix that.

While at it, make the parameter a kw_only argument so that such an error
not possible. Also, add a default value, since main_snapshot() doesn't
have a value to set.

Fixes: ad13ce19afd2 ('Propagate secret settings')

cda.py
@@ -51,7 +51,11 @@ def main_deploy_iso(cc: ClustersConfig, args: argparse.Namespace) -> None:
 
 
 def main_deploy(args: argparse.Namespace) -> None:
-    cc = ClustersConfig(args.config, args.secrets_path, args.worker_range)
+    cc = ClustersConfig(
+        args.config,
+        secrets_path=args.secrets_path,
+        worker_range=args.worker_range,
+    )
 
     if cc.kind == "openshift":
         main_deploy_openshift(cc, args)
@@ -61,7 +65,10 @@ def main_deploy(args: argparse.Namespace) -> None:
 
 def main_snapshot(args: argparse.Namespace) -> None:
     args = parse_args()
-    cc = ClustersConfig(args.config, args.worker_range)
+    cc = ClustersConfig(
+        args.config,
+        worker_range=args.worker_range,
+    )
 
     ais = AssistedInstallerService(cc.version, args.url)
     ai = AssistedClientAutomation(f"{args.url}:8090")

clustersConfig.py
@@ -170,7 +170,13 @@ class ClustersConfig:
     # Used to warn the user to change their config.
     deprecated_configs: dict[str, Optional[str]] = {"api_ip": "api_vip", "ingress_ip": "ingress_vip"}
 
-    def __init__(self, yaml_path: str, secrets_path: str, worker_range: common.RangeList = common.RangeList.UNLIMITED):
+    def __init__(
+        self,
+        yaml_path: str,
+        *,
+        secrets_path: str = "",
+        worker_range: common.RangeList = common.RangeList.UNLIMITED,
+    ):
         self._cluster_info: Optional[ClusterInfo] = None
         self._load_full_config(yaml_path)
         self._check_deprecated_config()