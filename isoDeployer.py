900940c1a46af12805d2c8120ff1a472178e1ad1
Thomas Haller <thaller@redhat.com>
2024-09-10 17:45:55 +0200

    clustersConfig: improve validation for network_api_port, sno, bf and "iso" cluster
    - drop IsoDeployer._validate(). This validation is now done by
      ClustersConfig.
    
    - ClustersConfig.network_api_port now returns an Optional[str]. Also,
      the validation now checks in various cases that the port is explicitly
      set, so that callers can rely on it and use unwrap().
    
    - for SNO clusters, the master must have an IP address. Validate that
      already in clustersConfig.
    
    - if we have any "bf" nodes, then currently they all must be of such
      kind. Move that check the clustersConfig.
    
    - move parsing the OCP version to ClusterConfig and validate that the
      "version" looks like a major OCP version.

--- isoDeployer.py
+++ isoDeployer.py
@@ -7,42 +7,25 @@ import ipu
 from baseDeployer import BaseDeployer
 from clustersConfig import ClustersConfig
 from concurrent.futures import ThreadPoolExecutor
-import sys
 from ktoolbox.common import unwrap
 
 
 class IsoDeployer(BaseDeployer):
     def __init__(self, cc: ClustersConfig, steps: list[str]):
         super().__init__(cc, steps)
-
-        if len(self._cc.masters) != 1:
-            logger.error("Masters must be of length one for deploying from iso")
-            sys.exit(-1)
-        self._master = self._cc.masters[0]
+        self._master = self._cc.cluster_config.single_master
         self._futures[self._master.name] = common.empty_future(host.Result)
-        self._validate()
-
-    def _validate(self) -> None:
-        if self._master.mac is None:
-            logger.error_and_exit(f"No MAC address provided for cluster {self._cc.name}, exiting")
-        if self._master.ip is None:
-            logger.error_and_exit(f"No IP address provided for cluster {self._cc.name}, exiting")
-        if self._master.name is None:
-            logger.error_and_exit(f"No name provided for cluster {self._cc.name}, exiting")
-        if not self._cc.network_api_port or self._cc.network_api_port == "auto":
-            logger.error_and_exit(f"Network API port with connection to {self._cc.name} must be specified, exiting")
 
     def deploy(self) -> None:
-        if self._cc.masters:
-            if PRE_STEP in self.steps:
-                self._preconfig()
-            else:
-                logger.info("Skipping pre configuration.")
+        if PRE_STEP in self.steps:
+            self._preconfig()
+        else:
+            logger.info("Skipping pre configuration.")
 
-            if MASTERS_STEP in self.steps:
-                self._deploy_master()
-            else:
-                logger.info("Skipping master creation.")
+        if MASTERS_STEP in self.steps:
+            self._deploy_master()
+        else:
+            logger.info("Skipping master creation.")
 
         if POST_STEP in self.steps:
             self._postconfig()
@@ -51,12 +34,11 @@ class IsoDeployer(BaseDeployer):
 
     def _deploy_master(self) -> None:
         if self._master.kind == "ipu":
-            node = ipu.IPUClusterNodeVersion(self._master, self._cc.get_external_port(), self._cc.network_api_port)
+            node = ipu.IPUClusterNodeVersion(self._master, self._cc.get_external_port(), unwrap(self._cc.cluster_config.network_api_port))
             executor = ThreadPoolExecutor(max_workers=len(self._cc.masters))
             node.start(unwrap(self._cc.cluster_config.install_iso), executor)
             node.future.result()
         elif self._master.kind == "marvell-dpu":
             isoCluster.MarvellIsoBoot(self._cc, self._master, unwrap(self._cc.cluster_config.install_iso))
         else:
-            logger.error("Not tested")
-            sys.exit(-1)
+            raise ValueError(f"unexpected master kind {self._master.kind}")
