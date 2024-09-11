bn222/cluster-deployment-automation

commit ad13ce19afd2e76033a7dd188f848413d553f279

Propagate secret settings

microshift.py
@@ -63,8 +63,8 @@ def cleanup_compose_cli(h: host.Host) -> None:
 
 
 # uses jinja to generate a kickstart file
-def generate_kickstart(rhel_number: str, uname_m: str) -> None:
-    with open('pull_secret.json', 'r') as f_in:
+def generate_kickstart(rhel_number: str, uname_m: str, secrets_path: str) -> None:
+    with open(secrets_path, 'r') as f_in:
         file_contents = f_in.read()
 
     ssh_pub, _, _ = next(common.iterate_ssh_keys(), (None, None, None))
@@ -143,13 +143,13 @@ def wait_for_build_to_finish(h: host.Host, build_id_two: Optional[str] = None) -
 
 
 # generates an iso file that builds microshift
-def iso_builder(h: host.Host, name_of_final_iso: str, version: str) -> None:
+def iso_builder(h: host.Host, name_of_final_iso: str, secrets_path: str, version: str) -> None:
     rhel_number = '9'
     cmd = "uname -m"
     uname_m = h.run(cmd).out.strip()
 
     cleanup_microshift(h, version)
-    generate_kickstart(rhel_number, uname_m)
+    generate_kickstart(rhel_number, uname_m, secrets_path)
 
     rhel_version = 'rhel9'
     rhel_name = 'RHEL 9'
@@ -216,14 +216,14 @@ rhsm = true'''.strip()
     shutil.rmtree("/var/cache/osbuild-worker/osbuild-store/stage/")
 
 
-def deploy(cluster_name: str, node: NodeConfig, external_port: str, version: str) -> None:
+def deploy(secrets_path: str, node: NodeConfig, external_port: str, version: str) -> None:
     lh = host.LocalHost()
     bmc = BMC.from_bmc(node.bmc, node.bmc_user, node.bmc_password)
     h = Host(node.node, bmc)
     name_of_final_iso = os.path.join(os.getcwd(), 'final.iso')
     login_uname = "redhat"
 
-    iso_builder(lh, name_of_final_iso, version)
+    iso_builder(lh, name_of_final_iso, secrets_path, version)
 
     if os.path.exists(f"{name_of_final_iso}"):
         logger.info(f"The file {name_of_final_iso} exists.")

cda.py
@@ -51,7 +51,7 @@ def main_deploy_iso(cc: ClustersConfig, args: argparse.Namespace) -> None:
 
 
 def main_deploy(args: argparse.Namespace) -> None:
-    cc = ClustersConfig(args.config, args.worker_range)
+    cc = ClustersConfig(args.config, args.secrets_path, args.worker_range)
 
     if cc.kind == "openshift":
         main_deploy_openshift(cc, args)

clustersConfig.py
@@ -164,21 +164,19 @@ class ClustersConfig:
     ntp_source: str = "clock.redhat.com"
     base_dns_domain: str = "redhat.com"
     install_iso: str = ""
+    secrets_path: str = ""
 
     # All configurations that used to be supported but are not anymore.
     # Used to warn the user to change their config.
     deprecated_configs: dict[str, Optional[str]] = {"api_ip": "api_vip", "ingress_ip": "ingress_vip"}
 
-    def __init__(
-        self,
-        yaml_path: str,
-        worker_range: common.RangeList = common.RangeList.UNLIMITED,
-    ):
+    def __init__(self, yaml_path: str, secrets_path: str, worker_range: common.RangeList = common.RangeList.UNLIMITED):
         self._cluster_info: Optional[ClusterInfo] = None
         self._load_full_config(yaml_path)
         self._check_deprecated_config()
 
         cc = self.fullConfig
+        self.secrets_path = secrets_path
         self.set_cc_defaults(cc)
         if "proxy" in cc:
             self.proxy = cc["proxy"]

extraConfigMicroshift.py
@@ -76,7 +76,7 @@ def ExtraConfigMicroshift(cc: ClustersConfig, cfg: ExtraConfigArgs, futures: dic
     # Set up pull secret
     logger.info(f"Copying pull secret to {acc.hostname()}:/etc/crio/openshift-pull-secret")
     acc.run("mkdir -p /etc/crio")
-    acc.copy_to("pull_secret.json", "/etc/crio/openshift-pull-secret")
+    acc.copy_to(cc.secrets_path, "/etc/crio/openshift-pull-secret")
     acc.run_or_die("chown root:root /etc/crio/openshift-pull-secret")
     acc.run_or_die("chmod 600 /etc/crio/openshift-pull-secret")
 

commit a86ad6207435c94015ab76e5505206ecb88a0924

clusterDeployer: fix passing secret_path to microshift.deploy()
Also, switch to use keyword arguments (and force using kw args on
microshift.deploy()) to avoid such errors in the future.

Fixes: ad13ce19afd2 ('Propagate secret settings')

clusterDeployer.py
@@ -230,7 +230,12 @@ class ClusterDeployer(BaseDeployer):
             version = match_to_proper_version_format(self._cc.version)
 
             if len(self._cc.masters) == 1:
-                microshift.deploy(self._cc.fullConfig["name"], self._cc.masters[0], self._cc.external_port, version)
+                microshift.deploy(
+                    secrets_path=self._secrets_path,
+                    node=self._cc.masters[0],
+                    external_port=self._cc.external_port,
+                    version=version,
+                )
             else:
                 logger.error_and_exit("Masters must be of length one for deploying microshift")
         if POST_STEP in self.steps:

microshift.py
@@ -216,7 +216,13 @@ rhsm = true'''.strip()
     shutil.rmtree("/var/cache/osbuild-worker/osbuild-store/stage/")
 
 
-def deploy(secrets_path: str, node: NodeConfig, external_port: str, version: str) -> None:
+def deploy(
+    *,
+    secrets_path: str,
+    node: NodeConfig,
+    external_port: str,
+    version: str,
+) -> None:
     lh = host.LocalHost()
     bmc = BMC.from_bmc(node.bmc, node.bmc_user, node.bmc_password)
     h = Host(node.node, bmc)