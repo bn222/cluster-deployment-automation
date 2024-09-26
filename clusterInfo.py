28fa48b824442ffbfcab2c0f0163bcc54042744d
Thomas Haller <thaller@redhat.com>
2024-09-26 20:08:43 +0200

    clusterInfo: show better error message loading cluster info
    - show more information about the failure and provide links
      how to setup the credentials.
    
    - raise an exception instead of a single logging line. The exception
      will contain a backtrace, which is going to be useful for debugging
      the cause. A crash due to an exception is useful (if we anyway want
      to abort in case of the error).

--- clusterInfo.py
+++ clusterInfo.py
@@ -1,9 +1,12 @@
-import sys
 import os
 import gspread
 import tenacity
 from oauth2client.service_account import ServiceAccountCredentials
 from logger import logger
+
+
+SHEET = "ANL lab HW enablement clusters and connections"
+URL = "https://docs.google.com/spreadsheets/d/1lXvcodJ8dmc_hcp0hzbPDU8t6-hCnAlEWFRdM2r_n0Q"
 
 
 class ClusterInfo:
@@ -34,7 +37,7 @@ def _default_cred_paths() -> list[str]:
 
 @tenacity.retry(wait=tenacity.wait_fixed(10), stop=tenacity.stop_after_attempt(5))
 def read_sheet() -> list[dict[str, str]]:
-    logger.info("Downloading sheet from Google")
+    logger.info(f"Reading cluster information from sheet {repr(SHEET)} ( {URL} )")
     scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
     cred_paths = _default_cred_paths()
     cred_path = None
@@ -42,12 +45,14 @@ def read_sheet() -> list[dict[str, str]]:
         if os.path.exists(e):
             cred_path = e
             break
-    if cred_path is None:
-        logger.info("Missing credentials.json while using templated config file")
-        sys.exit(-1)
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(cred_path, scopes)
-    file = gspread.auth.authorize(credentials)
-    sheet = file.open("ANL lab HW enablement clusters and connections")
+    try:
+        if cred_path is None:
+            raise ValueError(f"Credentials not found in {cred_paths}")
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(cred_path, scopes)
+        file = gspread.auth.authorize(credentials)
+        sheet = file.open(SHEET)
+    except Exception as e:
+        raise ValueError(f"Failure accessing google sheet: {e}. See https://docs.gspread.org/en/latest/oauth2.html#for-bots-using-service-account and share {repr(SHEET)} sheet ( {URL} )")
     sheet1 = sheet.sheet1
     return [{k: str(v) for k, v in record.items()} for record in sheet1.get_all_records()]
 
@@ -55,7 +60,6 @@ def read_sheet() -> list[dict[str, str]]:
 def load_all_cluster_info() -> dict[str, ClusterInfo]:
     cluster = None
     ret = []
-    logger.info("loading cluster information")
     for row in read_sheet():
         if row["Name"].startswith("Cluster"):
             if cluster is not None:
@@ -87,19 +91,15 @@ def load_all_cluster_info() -> dict[str, ClusterInfo]:
 
 def validate_cluster_info(cluster_info: ClusterInfo) -> None:
     if cluster_info.provision_host == "":
-        logger.info(f"Provision host missing for cluster {cluster_info.name}")
-        sys.exit(-1)
+        raise ValueError(f"Provision host missing for cluster {cluster_info.name}")
     if cluster_info.network_api_port == "":
-        logger.info(f"Network api port missing for cluster {cluster_info.name}")
-        sys.exit(-1)
+        raise ValueError(f"Network api port missing for cluster {cluster_info.name}")
     for e in cluster_info.workers:
         if e == "":
-            logger.info("Unnamed worker found for cluster {cluster_info.name}")
-            sys.exit(-1)
+            raise ValueError(f"Unnamed worker found for cluster {cluster_info.name}")
     for e in cluster_info.bmcs:
         if e == "":
-            logger.info("Unfilled IMPI address found for cluster {cluster_info.name}")
-            sys.exit(-1)
+            raise ValueError(f"Unfilled IMPI address found for cluster {cluster_info.name}")
 
 
 def load_cluster_info(provision_host: str) -> ClusterInfo:
