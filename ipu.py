bn222/cluster-deployment-automation

commit c80a04e0a839fa52fd664a9062dc41f14a53a282

ipu: fetch BMC "FirmwareVersion" via redfish HTTP API
No longer read the Redfish version via SSH ("/etc/issue" file).

  $ BMC_HOST="wsfd-advnetlab216-intel-ipu-imc.anl.eng.bos2.dc.redhat.com"; \
    python -c 'if 1:
        import ipu, sys

        b = ipu.IPUBMC(sys.argv[1])
        v = b.version()
        print(f">>> {repr(v)}")
    ' "$BMC_HOST"

https://issues.redhat.com/browse/IIC-387

ipu.py
@@ -1,4 +1,3 @@
-import sys
 import os
 import time
 from logger import logger
@@ -279,11 +278,12 @@ class IPUBMC(BMC):
             password = "calvincalvincalvin"
         super().__init__(full_url, user, password)
 
-    def _run_curl(self, command: str) -> None:
+    def _run_curl(self, command: str) -> host.Result:
         lh = host.LocalHost()
         logger.info(command)
         result = lh.run(f"curl -v -u {self.user}:{self.password} {command}")
         logger.info(result)
+        return result
 
     def _restart_redfish(self) -> None:
         rh = host.RemoteHost(self.url)
@@ -443,13 +443,22 @@ nohup sh -c '
         pass
 
     def version(self) -> str:
-        rh = host.RemoteHost(self.url)
-        rh.ssh_connect("root", password="", discover_auth=False)
-        contents = rh.read_file("/etc/issue")
-        match = re.search(r"Version: (\S+)", contents)
-        if match is None:
-            sys.exit(-1)
-        return match.group(1).strip()
+        url = f"https://{self.url}:8443/redfish/v1/Managers/1"
+        res = self._run_curl(f"-k '{url}'")
+        if res.returncode != 0:
+            raise RuntimeError(f"Cannot detect Redfish version: failure to fetch URL {repr(url)}")
+        try:
+            data = json.loads(res.out)
+        except Exception:
+            raise RuntimeError(f"Cannot detect Redfish version: not valid JSON received but {repr(res.out)}")
+        fwversion = data.get("FirmwareVersion")
+        if not isinstance(fwversion, str):
+            raise RuntimeError(f"Cannot detect Redfish version: FirmwareVersion field not present in {repr(res.out)}")
+        fwversion = fwversion.strip()
+        match = re.search(r"^MEV-.*\.([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+$", fwversion)
+        if not match:
+            raise RuntimeError(f"Cannot detect Redfish version: FirmwareVersion is unexpected {repr(fwversion)}")
+        return match.group(1)
 
 
 def extract_server(url: str) -> str:

commit 7126e9745921577442c92e5c5b64bc869e27d8c4

ipu: also check BMC "FirmwareVersion" via SSH
Redfish might not be running, so we need the fallback check for the
version via SSH.

https://github.com/bn222/cluster-deployment-automation/commit/c80a04e0a839fa52fd664a9062dc41f14a53a282#r148561539

Fixes: c80a04e0a839 ('ipu: fetch BMC "FirmwareVersion" via redfish HTTP API')

ipu.py
@@ -426,23 +426,37 @@ nohup sh -c '
     def cold_boot(self) -> None:
         pass
 
-    def version(self) -> str:
+    def _version_via_redfish(self) -> Optional[str]:
         url = f"https://{self.url}:8443/redfish/v1/Managers/1"
         res = self._run_curl(f"-k '{url}'")
         if res.returncode != 0:
-            raise RuntimeError(f"Cannot detect Redfish version: failure to fetch URL {repr(url)}")
+            return None
         try:
             data = json.loads(res.out)
         except Exception:
-            raise RuntimeError(f"Cannot detect Redfish version: not valid JSON received but {repr(res.out)}")
+            return None
         fwversion = data.get("FirmwareVersion")
         if not isinstance(fwversion, str):
-            raise RuntimeError(f"Cannot detect Redfish version: FirmwareVersion field not present in {repr(res.out)}")
-        fwversion = fwversion.strip()
-        match = re.search(r"^MEV-.*\.([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+$", fwversion)
+            return None
+        match = re.search(r"^MEV-.*\.([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+$", fwversion.strip())
         if not match:
-            raise RuntimeError(f"Cannot detect Redfish version: FirmwareVersion is unexpected {repr(fwversion)}")
+            return None
         return match.group(1)
+
+    def _version_via_ssh(self) -> Optional[str]:
+        rh = host.RemoteHost(self.url)
+        rh.ssh_connect("root", password="", discover_auth=False)
+        contents = rh.read_file("/etc/issue")
+        match = re.search(r"Version: (\S+)", contents)
+        if not match:
+            return None
+        return match.group(1).strip()
+
+    def version(self) -> str:
+        fwversion = self._version_via_redfish() or self._version_via_ssh()
+        if not fwversion:
+            raise RuntimeError(f"Failed to detect Redfish version on {self.url}")
+        return fwversion
 
 
 def extract_server(url: str) -> str: