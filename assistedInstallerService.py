5d4f66b288597f24db13ba47201d70de10e6e3b8
Balazs Nemeth <bnemeth@redhat.com>
2025-01-02 17:30:19 +0100

    Add check for hash in labels to allow upgrades

--- assistedInstallerService.py
+++ assistedInstallerService.py
@@ -345,13 +345,17 @@ class AssistedInstallerService:
         labels = j[0]["Labels"]
 
         pod_hash = hash_string(yaml.dump(pod))
-        cm_hash = hash_string(yaml.dump(cm))
-
+        if "cda-pod/hash" not in labels:
+            logger.warn(f"{name} running without label cda-pod/hash, stop needed")
         if labels["cda-pod/hash"] != pod_hash:
             logger.info(f"{name} pod running with different pod hash")
             logger.info(pod_hash)
             logger.info(labels["cda-pod/hash"])
             return True
+
+        cm_hash = hash_string(yaml.dump(cm))
+        if "cda-cm/hash" not in labels:
+            logger.warn(f"{name} running without label cda-cm/hash, stop needed")
         if labels["cda-cm/hash"] != cm_hash:
             logger.info(f"{name} pod running with different configmap hash")
             logger.info(f"New configmap hashmap is {cm_hash}")
