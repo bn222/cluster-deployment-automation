5886d08c5d1f2a2ac056b71453062c7a96000b12
Balazs Nemeth <bnemeth@redhat.com>
2025-01-10 17:21:13 +0100

    Don't coldboot

--- ipu.py
+++ ipu.py
@@ -121,9 +121,6 @@ class IPUClusterNode(ClusterNode):
 
     def post_boot(self, *, desired_ip_range: Optional[tuple[str, str]] = None) -> bool:
         logger.info("Workaround: cold booting the host since currently driver can't deal with host rebooting without coordination")
-        assert self.config.host_side_bmc is not None
-        ipu_host_bmc = BMC.from_bmc(self.config.host_side_bmc)
-        ipu_host_bmc.cold_boot()
         assert self.config.ip is not None
         acc = host.RemoteHost(self.config.ip)
         self._wait_for_acc_with_retry(acc=acc, timeout=300)
