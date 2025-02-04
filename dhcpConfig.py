bn222/cluster-deployment-automation

commit 13ae0145b6b4da91a66d2ef2cbcbacd063facd46

Fix black errors
Signed-off-by: Salvatore Daniele <sdaniele@redhat.com>

dhcpConfig.py
@@ -1,7 +1,3 @@
-import sys
-import os
-from pathlib import Path
-import shutil
 import ipaddress
 from dataclasses import dataclass, field
 import re
@@ -14,6 +10,7 @@ DEFAULT_NETMASK = "255.255.255.0"
 DHCPD_CONFIG_PATH = "/etc/dhcp/dhcpd.conf"
 DHCPD_CONFIG_BACKUP_PATH = "/etc/dhcp/dhcpd.conf.cda-backup"
 CDA_TAG = "Generated by CDA"
+
 
 @dataclass
 class DhcpdSubnetConfig:
@@ -50,14 +47,7 @@ class DhcpdHostConfig:
     fixed_address: str
 
     def to_string(self) -> str:
-        return (
-            f"# {CDA_TAG}\n"
-            f"host {self.hostname} {{\n"
-            f"    hardware ethernet {self.hardware_ethernet};\n"
-            f"    fixed-address {self.fixed_address};\n"
-            f"    option host-name {self.hostname};\n"
-            f"}}\n"
-        )
+        return f"# {CDA_TAG}\n" f"host {self.hostname} {{\n" f"    hardware ethernet {self.hardware_ethernet};\n" f"    fixed-address {self.fixed_address};\n" f"    option host-name {self.hostname};\n" f"}}\n"
 
 
 class DhcpConfig:
@@ -74,7 +64,7 @@ class DhcpConfig:
         self._subnet_configs.append(subnet)
 
     def _add_host_from_dhcpdhostconfig(self, host_config: DhcpdHostConfig) -> None:
-        self._subnet_configs.append(host_config)
+        self._host_configs.append(host_config)
 
     def add_host(self, hostname: str, hardware_ethernet: str, fixed_address: str) -> None:
         # Generate host / subnet configs for the current Node
@@ -94,10 +84,8 @@ class DhcpConfig:
 
         # Ensure existing hosts do not share addresses / hostname with the new entry
         for hc in self._host_configs:
-            if (new_hostconfig.hostname == hc.hostname or
-                new_hostconfig.fixed_address == hc.fixed_address or
-                new_hostconfig.hardware_ethernet == hc.hardware_ethernet):
-                    logger.error_and_exit(f"New host {new_hostconfig} overlaps with existing host {hc}")
+            if new_hostconfig.hostname == hc.hostname or new_hostconfig.fixed_address == hc.fixed_address or new_hostconfig.hardware_ethernet == hc.hardware_ethernet:
+                logger.error_and_exit(f"New host {new_hostconfig} overlaps with existing host {hc}")
 
         self._add_host_from_dhcpdhostconfig(new_hostconfig)
 
@@ -109,16 +97,13 @@ class DhcpConfig:
             config_str += host.to_string()
         return config_str
 
-    def write_to_file(self, file_path = DHCPD_CONFIG_PATH) -> None:
+    def write_to_file(self, file_path: str = DHCPD_CONFIG_PATH) -> None:
         with open(file_path, 'w') as file:
             file.write(self.to_string())
 
 
-def dhcp_config_from_file(file_path: str = None) -> DhcpConfig:
+def dhcp_config_from_file(file_path: str = DHCPD_CONFIG_PATH) -> DhcpConfig:
     config = DhcpConfig()
-
-    if not file_path:
-        file_path = DHCPD_CONFIG_PATH
 
     with open(file_path, 'r') as file:
         lines = file.readlines()
@@ -195,11 +180,7 @@ def dhcp_config_from_file(file_path: str = None) -> DhcpConfig:
         if host_match:
             if current_host is not None:
                 logger.error_and_exit(f"Malformed host in dhcpd config {file_path}")
-            current_host = {
-                'hostname': host_match.group(1),
-                'hardware_ethernet': None,
-                'fixed_address': None
-            }
+            current_host = {'hostname': host_match.group(1), 'hardware_ethernet': None, 'fixed_address': None}
             continue
 
         if current_host is not None:
@@ -214,9 +195,8 @@ def dhcp_config_from_file(file_path: str = None) -> DhcpConfig:
                 continue
 
             if '}' in line:
-                config._host_configs.append(DhcpdHostConfig(**current_host))
+                config._host_configs.append(DhcpdHostConfig(hostname=str(current_host['hostname']), hardware_ethernet=str(current_host['hardware_ethernet']), fixed_address=str(current_host['fixed_address'])))
                 current_host = None
-
     return config
 
 
@@ -251,4 +231,3 @@ def subnet_config_from_host_config(hc: DhcpdHostConfig) -> DhcpdSubnetConfig:
 def _convert_to_cidr(ipv4_address: str, subnet_mask: str) -> str:
     network = ipaddress.ip_network(f"{ipv4_address}/{subnet_mask}", strict=False)
     return str(network)
-

common.py
@@ -147,6 +147,7 @@ class IPRouteAddressEntry:
     def has_carrier(self) -> bool:
         return "NO-CARRIER" not in self.flags
 
+
 class HttpServerManager:
     def __init__(self, path: str, port: int = 8000):
         self.path = path
@@ -188,7 +189,6 @@ class HttpServerManager:
             logger.debug(f"port {self.port} in use, trying port + 1")
             port += 1
         return port
-
 
 
 def _parse_json_list(jstr: str, *, strict_parsing: bool = False) -> list[typing.Any]:

isoCluster.py
@@ -2,12 +2,8 @@ import sys
 import os
 from pathlib import Path
 import shutil
-import ipaddress
-from dataclasses import dataclass
-import re
 import time
 import urllib.parse
-from typing import Tuple
 from logger import logger
 from clustersConfig import ClustersConfig
 from clustersConfig import NodeConfig

commit 30f477124c70f6999622d5b4d135b936333cbd48

dhcpConfig: fix parsing dhcpd config file
The values might be None. Calling str(x) on that is wrong.

  2025-02-04 11:29:21 INFO [th:139951722780480] (dhcpConfig.py:265): Configuring dhcpd entry
  2025-02-04 11:29:21 DEBUG [th:139951722780480] (dhcpConfig.py:242): Rendering dhcpd conf
  2025-02-04 11:29:21 DEBUG [th:139951722780480] (dhcpConfig.py:248): Existing dhcpd configuration detected
  2025-02-04 11:29:21 DEBUG [th:139951722780480] (dhcpConfig.py:81): Subnet config for 172.16.3.16 already exists at /etc/dhcp/dhcpd.conf
  2025-02-04 11:29:21 ERROR [th:139951722780480] (logger.py:22): New host DhcpdHostConfig(hostname='42-acc', hardware_ethernet='', fixed_address='172.16.3.16') overlaps with existing host DhcpdHostConfig(hostname='42-acc', hardware_ethernet='None', fixed_address='172.16.3.16')

Fixes: 13ae0145b6b4 ('Fix black errors')

dhcpConfig.py
@@ -2,6 +2,7 @@ import ipaddress
 from dataclasses import dataclass, field
 import re
 from typing import Tuple
+from typing import Optional
 from logger import logger
 import common
 from pathlib import Path
@@ -125,7 +126,7 @@ def dhcp_config_from_file(file_path: str = DHCPD_CONFIG_PATH) -> DhcpConfig:
     ntp_servers_pattern = re.compile(r'\s*option\s+ntp-servers\s+([^;]+);')
 
     current_subnet = None
-    current_host = None
+    current_host: Optional[dict[str, str]] = None
 
     for line in lines:
         subnet_match = subnet_pattern.match(line)
@@ -185,7 +186,7 @@ def dhcp_config_from_file(file_path: str = DHCPD_CONFIG_PATH) -> DhcpConfig:
         if host_match:
             if current_host is not None:
                 logger.error_and_exit(f"Malformed host in dhcpd config {file_path}")
-            current_host = {'hostname': host_match.group(1), 'hardware_ethernet': None, 'fixed_address': None}
+            current_host = {'hostname': host_match.group(1), 'hardware_ethernet': '', 'fixed_address': ''}
             continue
 
         if current_host is not None:
@@ -200,7 +201,7 @@ def dhcp_config_from_file(file_path: str = DHCPD_CONFIG_PATH) -> DhcpConfig:
                 continue
 
             if '}' in line:
-                config._host_configs.append(DhcpdHostConfig(hostname=str(current_host['hostname']), hardware_ethernet=str(current_host['hardware_ethernet']), fixed_address=str(current_host['fixed_address'])))
+                config._host_configs.append(DhcpdHostConfig(hostname=current_host['hostname'], hardware_ethernet=current_host['hardware_ethernet'], fixed_address=current_host['fixed_address']))
                 current_host = None
     return config
 