c4aaf2f3f651d20d97f2cf34f998d25f10d02028
samd2021 <sadasilv@redhat.com>
2025-06-03 11:03:55 -0400

    extraConfigIsoBuilder.py: Add the kickstart generator for Image Mode
    This allows us to have the same kickstart behavior as rhel for edge.
    A cool bootc feature is that we can move some post boot things to the base image it
    self instead of in the kickstart. This means that we can have the same
    behavior in every deployment (base image) vs individual deployments (kickstart iso build time)
    
    We also consume rhc credentials in the Image Mode kickstart generator

---
+++ extraConfigIsoBuilder.py
@@ -0,0 +1,30 @@
+import common
+from logger import logger
+import sys
+from jinja2 import Template
+import os
+
+
+# uses jinja to generate a kickstart file
+def generate_kickstart_image_mode(secrets_path: str, final_kickstart: str, rhc_org_id: str, rhc_act_key: str) -> None:
+    with open(secrets_path, 'r') as f_in:
+        file_contents = f_in.read()
+
+    ssh_pub, _, _ = next(common.iterate_ssh_keys(), (None, None, None))
+    if ssh_pub is not None:
+        with open(ssh_pub, 'r') as ssh_in:
+            ssh_contents = ssh_in.read()
+    else:
+        logger.error("No ssh keys found in generate_kickstart")
+        sys.exit(-1)
+
+    template_file = "kickstart-image-mode.ks.j2"
+    with open(template_file, 'r') as f:
+        lines = f.read()
+
+    with open(final_kickstart, 'w') as f_out:
+        template = Template(lines)
+        f_out.write(template.render(pull_secret=file_contents, ssh_key=ssh_contents, rhc_org_id=rhc_org_id, rhc_act_key=rhc_act_key))
+    if not os.path.exists(final_kickstart):
+        logger.error_and_exit(f"Expected generated kickstart not found at: {final_kickstart}")
+    logger.debug(f"Kickstart generated at {final_kickstart}")
