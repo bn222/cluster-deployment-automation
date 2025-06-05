bn222/cluster-deployment-automation

commit 969a134a5b4e11aad3a0cf349e0026deecceb4db

unwrap from general structure

isoDeployer.py
@@ -66,9 +66,7 @@ class IsoDeployer(BaseDeployer):
         dpu_kind = detect_dpu(self._master)
         if dpu_kind == "ipu":
             node = ipu.IPUClusterNode(self._master, self._cc.get_external_port(), self._cc.network_api_port)
-            executor = ThreadPoolExecutor(max_workers=len(self._cc.masters))
-            future = executor.submit(node.start, self._cc.install_iso)
-            future.result()
+            node.start(self._cc.install_iso)
             node.post_boot()
         elif dpu_kind == "marvell":
             marvell.MarvellIsoBoot(self._master, self._cc.install_iso)

commit e6d9926c31fee7066b0aac8fb508b85ab8f9fd54

isoDeployer: drop unused import for ThreadPoolExecutor
Fixes: 969a134a5b4e ('unwrap from general structure')

isoDeployer.py
@@ -6,7 +6,6 @@ import marvell
 import ipu
 from baseDeployer import BaseDeployer
 from clustersConfig import ClustersConfig
-from concurrent.futures import ThreadPoolExecutor
 from dpuVendor import detect_dpu
 import sys
 import dhcpConfig