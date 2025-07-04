7fa05aa2a58f21516cc001b7102e456fdef50368
Balazs Nemeth <bnemeth@redhat.com>
2025-07-04 08:44:02 +0200

    Revert "Merge pull request #364 from SamD2021/hostpath-implementation"
    This reverts commit b113343fc8a7791a04792f618e3db3c2636d1db3, reversing
    changes made to da83d7011d434a980a4f5bbf4f14b4d124a2495d.

--- extraConfigImageRegistry.py
+++ extraConfigImageRegistry.py
@@ -1,55 +1,21 @@
+from clustersConfig import ClustersConfig
+from imageRegistry import InClusterRegistry
 from concurrent.futures import Future
 from typing import Optional
 from logger import logger
-from clustersConfig import ClustersConfig, ExtraConfigArgs
+from clustersConfig import ExtraConfigArgs
 import host
-from imageRegistry import InClusterRegistry, LocalRegistry, RegistryType
-from clusterStorage import create_cluster_storage, StorageType
 
 
 def ExtraConfigImageRegistry(cc: ClustersConfig, cfg: ExtraConfigArgs, futures: dict[str, Future[Optional[host.Result]]]) -> None:
     [f.result() for (_, f) in futures.items()]
     logger.info("Running post config step to enable cluster registry")
 
-    if cfg.registry_type == RegistryType.IN_CLUSTER.value:
-        # Reference documentation:
-        # https://docs.openshift.com/container-platform/4.15/registry/configuring-registry-operator.html
+    # Reference documentation:
+    # https://docs.openshift.com/container-platform/4.15/registry/configuring-registry-operator.html
 
-        # Get registry node information
-        registry_node = cc.get_registry_storage_node()
-
-        logger.info(f"Using node '{registry_node.name}' for registry storage")
-
-        # Create ClusterStorage instance targeting the specific registry node
-        from clusterStorage import HostPathStorage
-
-        cluster_storage = create_cluster_storage(kubeconfig_path=cc.kubeconfig, storage_type=StorageType.HOSTPATH, target_node_hostname=registry_node.name, storage_path="/var/lib/registry-storage")
-        # Cast to specific type to access HostPath-specific methods
-        hostpath_storage = cluster_storage if isinstance(cluster_storage, HostPathStorage) else None
-        if not hostpath_storage:
-            logger.error_and_exit("Expected HostPathStorage implementation for registry storage")
-        in_cluster_reg = InClusterRegistry(kubeconfig=cc.kubeconfig, storage_class=cluster_storage.get_storage_class_name(), storage=hostpath_storage)
-
-        logger.info("Redeploying in-cluster registry...")
-        in_cluster_reg.undeploy()
-        cluster_storage.undeploy_storage()
-        # Deploy storage foundation (StorageClass + directories)
-        logger.info("Deploying storage foundation...")
-        cluster_storage.deploy_storage()
-
-        # Create persistent volume for registry storage
-        logger.info("Creating persistent volume for registry storage...")
-        registry_storage_size = cc.get_registry_storage_node().in_cluster_registry_storage_size
-        hostpath_storage.create_pv_with_node_affinity(pv_name="registry-pv", storage_size=registry_storage_size, storage_path="/var/lib/registry-storage")
-
-        # Deploy in-cluster registry with configurable storage size
-        logger.info("Deploying in-cluster registry...")
-        in_cluster_reg.deploy()
-
-    elif cfg.registry_type == RegistryType.LOCAL.value:
-        lh = host.LocalHost()
-        local_reg = LocalRegistry(lh)
-        local_reg.deploy()
+    reg = InClusterRegistry(cc.kubeconfig)
+    reg.deploy()
 
 
 def main() -> None:
--- extraConfigSriov.py
+++ extraConfigSriov.py
@@ -11,7 +11,7 @@ import common
 from typing import Optional
 from logger import logger
 from clustersConfig import ExtraConfigArgs
-from imageRegistry import LocalRegistry
+from imageRegistry import ImageRegistry
 from reglocal import git_build_local, GitBuildLocalContainerInfo
 from common import git_repo_setup
 
@@ -73,7 +73,7 @@ def _sno_make_deploy(
         logger.info(f"Image {image} provided to load custom sriov-network-operator")
         deploy_env["SRIOV_NETWORK_OPERATOR_IMAGE"] = image
     elif build_local:
-        imgReg = LocalRegistry(rsh)
+        imgReg = ImageRegistry(rsh)
         imgReg.deploy()
         registry = imgReg.get_url()
         project = "openshift-sriov-network-operator"
