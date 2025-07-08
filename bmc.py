bn222/cluster-deployment-automation

commit f857f85a82df7d1928b186c2284f7e45712831be

Remove redundant Dell specific information from BMC class
- The Redfish API does not need the endpoint information
- The Redfish API only requires an ip address or hostname
- The Redfish API's model parameter is redundant since the Redfish
will determine the model by Manufacturer information reported.

For example:
```
import host
from nfs import NFS
from bmc import BMC
import os
from ailib import Redfish

lh = host.LocalHost()
nfs = NFS(lh, "eno8303")
iso = "RHEL-9.6.0-20250416.d.7-x86_64-dvd1-with-ks.iso"
iso = nfs.host_file(os.path.join(os.getcwd(), iso))
red = Redfish("wsfd-advnetlab229-intel-ptl-bmc.anl.eng.bos2.dc.redhat.com", "root", "0penBmc", insecure=True, debug=True)

red.eject_iso()
red.insert_iso(iso)
red.set_iso_once()
red.restart()
```

Signed-off-by: William Zhao <wizhao@redhat.com>

bmc.py
@@ -25,16 +25,10 @@ class BMC:
         return BMC.from_bmc(bmc_config.url, bmc_config.user, bmc_config.password)
 
     @staticmethod
-    def from_url(url: str, user: str = "root", password: str = "calvin") -> 'BMC':
-        url = f"{url}/redfish/v1/Systems/System.Embedded.1"
-        return BMC(url, user, password)
-
-    @staticmethod
     def from_bmc(ip_or_hostname: str, user: str = "root", password: str = "calvin") -> 'BMC':
         if ip_or_hostname == "":
             raise ValueError("BMC not defined")
-        url = f"https://{ip_or_hostname}/redfish/v1/Systems/System.Embedded.1"
-        return BMC(url, user, password)
+        return BMC(ip_or_hostname, user, password)
 
     """
     Red Fish is used to boot ISO images with virtual media.
@@ -130,7 +124,7 @@ class BMC:
         logger.error_and_exit(f"Redfish didn't come up after {t} time")
 
     def _redfish(self) -> Redfish:
-        return Redfish(self.url, self.user, self.password, model='dell', debug=False)
+        return Redfish(self.url, self.user, self.password, debug=False)
 
     def stop(self) -> None:
         self._redfish().stop()

commit 4cec2332228681b54b41d3e22af89323cf85ba19

bmc: cleanup hostname handling for BMC
We must be clear whether this is a "BMC.url" or a "BMC.bmc_host". That
is, is it a URL or just a hostname for which we (at certain places) need
to append a "https://" scheme. Otherwise, we get errors like

    requests.exceptions.MissingSchema: Invalid URL '10.26.16.95': No scheme supplied. Perhaps you meant https://10.26.16.95?

Rename the "url" attribute to "bmc_host" to make this clear. Also, the
various callers will instead use the "base_url" property, as
appropriate.

See-also: https://jenkins-csb-nst-net-hw-ci.dno.corp.redhat.com/job/99_E2E_Marvell_DPU_Deploy/593/console

Fixes: f857f85a82df ('Remove redundant Dell specific information from BMC class')

bmc.py
@@ -14,27 +14,34 @@ class BmcConfig:
 
 
 class BMC:
-    def __init__(self, full_url: str, user: str = "root", password: str = "calvin"):
-        self.url = full_url
+    def __init__(
+        self,
+        bmc_host: str,
+        user: str = "root",
+        password: str = "calvin",
+        *,
+        port: int = 443,
+    ):
+        if not bmc_host:
+            raise ValueError("BMC host not defined")
+        self.bmc_host = bmc_host
         self.user = user
         self.password = password
-        logger.info(f"{full_url} {user} {password}")
+        self.port = port
 
-    @property
-    def base_url(self) -> str:
-        scheme = ""
-        if not self.url.startswith("https://") and not self.url.startswith("http://"):
-            scheme = "https://"
-        return f"{scheme}{self.url}"
+        url = f"https://{self.bmc_host}"
+        if self.port != 443:
+            url = f"{url}:{self.port}"
+        self.base_url = url
+
+        logger.info(f"BMC: {bmc_host} {user} {password}")
 
     @staticmethod
     def from_bmc_config(bmc_config: BmcConfig) -> 'BMC':
-        return BMC.from_bmc(bmc_config.url, bmc_config.user, bmc_config.password)
+        return BMC(bmc_config.url, bmc_config.user, bmc_config.password)
 
     @staticmethod
     def from_bmc(ip_or_hostname: str, user: str = "root", password: str = "calvin") -> 'BMC':
-        if ip_or_hostname == "":
-            raise ValueError("BMC not defined")
         return BMC(ip_or_hostname, user, password)
 
     """
@@ -72,7 +79,7 @@ class BMC:
     def boot_iso_redfish(self, iso_path: str, retries: int = 10, retry_delay: int = 60) -> None:
         def boot_iso_with_retry(iso_path: str, attempt: int) -> None:
             logger.info(iso_path)
-            logger.info(f"Trying to boot {self.url} using {iso_path}, attempt {attempt}")
+            logger.info(f"Trying to boot {self.bmc_host} using {iso_path}, attempt {attempt}")
             red = self._redfish()
             try:
                 red.eject_iso()
@@ -90,7 +97,7 @@ class BMC:
             logger.info("setting to boot from iso")
             red.restart()
             time.sleep(10)
-            logger.info(f"Finished sending boot to {self.url}")
+            logger.info(f"Finished sending boot to {self.bmc_host}")
 
         assert ":" in iso_path
         for attempt in range(retries):
@@ -124,7 +131,7 @@ class BMC:
             t = timer.Timer("10m")
 
             while not t.triggered():
-                response = requests.get(self.url, auth=(self.user, self.password), verify=False, timeout=5)
+                response = requests.get(self.base_url, auth=(self.user, self.password), verify=False, timeout=5)
                 if response.status_code == 200:
                     logger.info("Redfish reset completed")
                     return
@@ -134,7 +141,7 @@ class BMC:
             logger.error_and_exit(f"Redfish didn't come up after {t} time")
 
     def _redfish(self) -> Redfish:
-        return Redfish(self.url, self.user, self.password, debug=False)
+        return Redfish(self.bmc_host, self.user, self.password, debug=False)
 
     def stop(self) -> None:
         self._redfish().stop()

host.py
@@ -136,7 +136,7 @@ class AutoLogin(Login):
 
 class Host:
     def __new__(cls, hostname: str, bmc: Optional[BMC] = None) -> 'Host':
-        key = (hostname, bmc.url if bmc else None)
+        key = (hostname, bmc.bmc_host if bmc else None)
         if key not in host_instances:
             host_instances[key] = super().__new__(cls)
         return host_instances[key]

ipu.py
@@ -198,7 +198,12 @@ class IPUBMC(BMC):
             password = "calvincalvincalvin"
         else:
             password = bmc_config.password
-        super().__init__(bmc_config.url, bmc_config.user, password)
+        super().__init__(
+            bmc_config.url,
+            bmc_config.user,
+            password,
+            port=8443,
+        )
         self._host_bmc = host_bmc
 
     def prepared(self, imc: host.Host) -> bool:
@@ -342,7 +347,7 @@ fi
             return sha("".join(f.readlines()))
 
     def _create_imc_rsh(self) -> host.Host:
-        rsh = host.RemoteHost(self.url)
+        rsh = host.RemoteHost(self.bmc_host)
         rsh.ssh_connect("root", password="", discover_auth=False)
         return rsh
 
@@ -422,8 +427,8 @@ fi
         except requests.exceptions.RequestException as e:
             logger.error(f"Request failed: {e}")
 
-    def _redfish_available(self, url: str) -> bool:
-        full_url = f"https://{url}:8443/redfish/v1/Systems/1"
+    def _redfish_available(self) -> bool:
+        full_url = f"{self.base_url}/redfish/v1/Systems/1"
         try:
             response = requests.get(full_url, auth=(self.user, self.password), verify=False)
             response.raise_for_status()
@@ -434,7 +439,7 @@ fi
             return False
 
     def _virtual_media_is_inserted(self, filename: str) -> bool:
-        url = f"https://{self.url}:8443/redfish/v1/Systems/1/VirtualMedia/1"
+        url = f"{self.base_url}/redfish/v1/Systems/1/VirtualMedia/1"
         data = self._requests_get(url)
         inserted = data.get("Inserted")
         if not isinstance(inserted, bool) or not inserted:
@@ -469,18 +474,18 @@ fi
         logger.info(f"Done iso_path {repr(iso_path)} is inserted as VirtualMedia, took {t.elapsed()}")
 
     def _insert_media(self, iso_path: str, *, expected_size: int) -> None:
-        url = f"https://{self.url}:8443/redfish/v1/Systems/1/VirtualMedia/1/Actions/VirtualMedia.InsertMedia"
+        url = f"{self.base_url}/redfish/v1/Systems/1/VirtualMedia/1/Actions/VirtualMedia.InsertMedia"
         data = {"Image": iso_path, "TransferMethod": "Upload"}
         self._requests_post(url, data)
         self._wait_iso_downloaded(iso_path, expected_size=expected_size)
 
     def _bootsource_override_cd(self) -> None:
-        url = f"https://{self.url}:8443/redfish/v1/Systems/1"
+        url = f"{self.base_url}/redfish/v1/Systems/1"
         data = {"Boot": {"BootSourceOverrideEnabled": "Once", "BootSourceOverrideTarget": "Cd"}}
         self._requests_patch(url, data)
 
     def _reboot(self) -> None:
-        url = f"https://{self.url}:8443/redfish/v1/Managers/1/Actions/Manager.Reset"
+        url = f"{self.base_url}/redfish/v1/Managers/1/Actions/Manager.Reset"
         data = {"ResetType": "ForceRestart"}
         self._requests_post(url, data)
 
@@ -497,7 +502,7 @@ fi
         time.sleep(20)
 
     def _redfish_version(self) -> str:
-        url = f"https://{self.url}:8443/redfish/v1/Managers/1"
+        url = f"{self.base_url}/redfish/v1/Managers/1"
         data = self._requests_get(url)
         fwversion = data.get("FirmwareVersion")
         if not isinstance(fwversion, str):
@@ -508,7 +513,7 @@ fi
         return match.group(1)
 
     def _redfish_name(self) -> str:
-        url = f"https://{self.url}:8443/redfish/v1/"
+        url = f"{self.base_url}/redfish/v1/"
         data = self._requests_get(url)
         name = data.get("Name")
         if not isinstance(name, str):
@@ -516,7 +521,7 @@ fi
         return name
 
     def _version_via_ssh(self) -> Optional[str]:
-        rh = host.RemoteHost(self.url)
+        rh = host.RemoteHost(self.bmc_host)
         # TODO: after mev ts upgrade, remove the timeout + try/except
         try:
             rh.ssh_connect("root", password="", discover_auth=False, timeout="5s")
@@ -531,7 +536,7 @@ fi
         return match.group(1).strip()
 
     def version(self) -> str:
-        if self._redfish_available(self.url):
+        if self._redfish_available():
             return self._redfish_version()
         else:
             fwversion = self._version_via_ssh()
@@ -540,12 +545,12 @@ fi
             return fwversion
 
     def is_ipu(self) -> bool:
-        logger.info(f"Checking if DPU is IPU via {self.url}")
-        if self._redfish_available(self.url):
+        logger.info(f"Checking if DPU is IPU via {self.bmc_host}")
+        if self._redfish_available():
             return "Intel IPU" in self._redfish_name()
         else:
             # workaround: remove when redfish is started properly at boot
-            logger.info(f"Redfish is not up on {self.url}, using SSH to check")
+            logger.info(f"Redfish is not up on {self.bmc_host}, using SSH to check")
             return self._version_via_ssh() is not None
 
     def ensure_firmware(self, force: bool, version: str) -> None:
@@ -559,9 +564,9 @@ fi
                 return False
 
         assert self.is_ipu()
-        imc = host.Host(self.url)
+        imc = host.Host(self.bmc_host)
 
-        logger.info(f"Will ensure {self.url} is on firmware version: {version}")
+        logger.info(f"Will ensure {self.bmc_host} is on firmware version: {version}")
         logger.info("Checking if firmware update is required")
 
         if force or (not firmware_is_same()):
@@ -574,7 +579,7 @@ fi
         lh = host.LocalHost()
 
         logger.info("Starting flash of SSD/SPI (this will take some time ~40min)")
-        fw_up_cmd = f"--dpu-type ipu --imc-address {self.url} firmware up --version {version}"
+        fw_up_cmd = f"--dpu-type ipu --imc-address {self.bmc_host} firmware up --version {version}"
         ret = lh.run_in_container(fw_up_cmd, interactive=True)
 
         if not ret.success():