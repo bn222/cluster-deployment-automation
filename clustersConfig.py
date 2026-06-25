bn222/cluster-deployment-automation

commit 1916be7d60655bd50700a0ea12dc56cde53aedf3

Handle case when virsh network does not yet exist
The first time we run CDA on a freshly installed cluster, the default
network may not exist yet.

i.e.

error: failed to connect to the hypervisor
error: Operation not supported: Cannot use direct socket mode if no URI is set. For more information see https://libvirt.org/kbase/failed_connection_after_install
.html

This will cause the following to fail:

"""
xml_str = hostconn.run("virsh net-dumpxml default").out
tree = et.fromstring(xml_str)
"""

Signed-off-by: Salvatore Daniele <sdaniele@redhat.com>

clustersConfig.py
@@ -245,7 +245,7 @@ class ClustersConfig:
 
         # Update the last IP based on the config.
         for node in self.all_nodes():
-            if node.ip and ipaddress.IPv4Address(node.ip) > ipaddress.IPv4Address(last_ip):
+            if node.ip and last_ip and ipaddress.IPv4Address(node.ip) > ipaddress.IPv4Address(last_ip):
                 last_ip = node.ip
 
         if last_ip and ipaddress.IPv4Address(last_ip) > ipaddress.IPv4Address(ip_range[0]) + n_nodes:
@@ -260,17 +260,19 @@ class ClustersConfig:
         self.local_bridge_config = BridgeConfig(ip=self.ip_range[0], mask=ip_mask, dynamic_ip_range=dynamic_ip_range)
         self.remote_bridge_config = BridgeConfig(ip=ip_range[1], mask=ip_mask)
 
-    def get_last_ip(self) -> str:
+    def get_last_ip(self) -> str | None:
         hostconn = host.LocalHost()
         last_ip = "0.0.0.0"
         xml_str = hostconn.run("virsh net-dumpxml default").out
-        tree = et.fromstring(xml_str)
-        ip_tree = next((it for it in tree.iter("ip")), et.Element(''))
-        dhcp = next((it for it in ip_tree.iter("dhcp")), et.Element(''))
-        for e in dhcp:
-            if ipaddress.IPv4Address(e.get('ip', "0.0.0.0")) > ipaddress.IPv4Address(last_ip):
-                last_ip = e.get('ip', "0.0.0.0")
-        return last_ip
+        if xml_str.strip():
+            tree = et.fromstring(xml_str)
+            ip_tree = next((it for it in tree.iter("ip")), et.Element(''))
+            dhcp = next((it for it in ip_tree.iter("dhcp")), et.Element(''))
+            for e in dhcp:
+                if ipaddress.IPv4Address(e.get('ip', "0.0.0.0")) > ipaddress.IPv4Address(last_ip):
+                    last_ip = e.get('ip', "0.0.0.0")
+            return last_ip
+        return None
 
     def set_cc_defaults(self, cc: dict[str, Union[None, str, list[dict[str, str]]]]) -> None:
         # Some config may be left out from the yaml. Try to provide defaults.

commit f548c15418d1642764faf6d0bd104b7ee6fd5983

clustersConfig: Fix IP range calculation when network doesn't exist
On a fresh system where the libvirt default network doesn't exist yet,
get_last_ip() returns None. The subsequent loop that updates last_ip
based on node IPs from the config has the condition:

    if node.ip and last_ip and ...

Since None is falsy, this condition was always False, meaning the
configured node IPs (e.g., 192.168.122.141-145) were never considered.
The range would be calculated as just ip_range[0] + n_nodes (e.g.,
192.168.122.1-7), causing the error:
"Node IP not in cluster subnet range"

Fix by defaulting last_ip to "0.0.0.0" when the network doesn't exist,
allowing the loop to properly consider node IPs from the config.

Fixes: 1916be7d6065 ("Handle case when virsh network does not yet exist")
Signed-off-by: Maxime Coquelin <maxime.coquelin@redhat.com>

clustersConfig.py
@@ -338,7 +338,8 @@ class ClustersConfig:
         n_nodes = len(cc["masters"]) + len(cc["workers"]) + 1
 
         # Get the last IP used in the running cluster.
-        last_ip = self.get_last_ip()
+        # Default to 0.0.0.0 if network doesn't exist, so we still consider node IPs from config.
+        last_ip = self.get_last_ip() or "0.0.0.0"
 
         # Update the last IP based on the config.
         for node in self.all_nodes():