d0668386d8fa4aeabee6a5d9038c866b0aacab4f
Ales Musil <amusil@redhat.com>
2026-08-03 10:18:42 +0200

    Add support for 5.0 nightly and ec
    Signed-off-by: Ales Musil <amusil@redhat.com>

--- assistedInstallerService.py
+++ assistedInstallerService.py
@@ -382,6 +382,20 @@ class AssistedInstallerService:
                 'url': self.get_normal_pullspec(version),
                 'version': version,
             }
+        elif re.search(r'5\.0\.0-nightly', version):
+            ret = {
+                'openshift_version': '5.0-multi',
+                'cpu_architectures': ['x86_64', 'arm64', 'ppc64le', 's390x'],
+                'url': self.get_nightly_pullspec(version),
+                'version': version,
+            }
+        elif re.search(r'5\.0\.0-ec.[0-9]+', version):
+            ret = {
+                'openshift_version': '5.0-multi',
+                'cpu_architectures': ['x86_64', 'arm64', 'ppc64le', 's390x'],
+                'url': self.get_normal_pullspec(version),
+                'version': version,
+            }
         else:
             logger.error(f"Unknown version {version}")
             sys.exit(-1)
